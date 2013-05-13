a100e2ac055ab8f37a1c32817801788396bf8194
Mitch Garnaat <mitch@garnaat.com>
2013-05-13 16:25:20 -0700

    Initial version of a simple events module.

--- botocore/events.py
+++ botocore/events.py
@@ -1,4 +1,5 @@
-# Copyright 2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright (c) 2012-2013 Mitch Garnaat http://garnaat.org/
+# Copyright 2012-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the
@@ -19,38 +20,20 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 #
-"""Builtin event handlers.
 
-This module contains builtin handlers for events emitted by botocore.
-"""
+AllEvents = {
+    'after-call': '.%s.%s',
+    'after-parsed': '.%s.%s.%s.%s',
+    'before-call': '.%s.%s',
+    'service-created': None
+    }
 
-import base64
-import json
-import six
-from botocore.compat import unquote
-
-
-def decode_console_output(**kwargs):
-    try:
-        value = base64.b64decode(six.b(kwargs['value'])).decode('utf-8')
-    except:
-        value = kwargs['value']
-    return value
-
-
-def decode_jsondoc(**kwargs):
-    try:
-        value = json.loads(unquote(kwargs['value']))
-    except:
-        value = kwargs['value']
-    return value
-
-# This is a list of (event_name, handler).
-# When a Session is created, everything in this list will be
-# automatically registered with that Session.
-BUILTIN_HANDLERS = [
-    ('after-parsed.ec2.GetConsoleOutput.String.Output',
-     decode_console_output),
-    ('after-parsed.iam.*.policyDocumentType.PolicyDocument',
-     decode_jsondoc)
-]
+def create_event(event_name, *fmtargs):
+    if event_name in AllEvents:
+        fmt_string = AllEvents[event_name]
+        if fmt_string:
+            event = event_name + (fmt_string % fmtargs)
+        else:
+            event = event_name
+        return event
+    raise ValueError('Unknown event: %s' % event_name)
