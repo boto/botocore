eff2f29e8603dad828e228dce4f902efe19ae9ae
James Saryerwinnie <js@jamesls.com>
2013-10-12 10:01:05 -0700

    Reintroduce the upgrade to requests 2.0.0
    Should be good to go now that we don't need httpretty
    in the CLI tests.

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ packages = [
     'botocore',
 ]
 
-requires = ['requests==1.2.3',
+requires = ['requests==2.0.0',
             'six>=1.1.0',
             'jmespath==0.0.3',
             'python-dateutil>=2.1']
