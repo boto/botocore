b85890cb47c70f642e740d8bc958d7647c039eec
Mitch Garnaat <mitch@garnaat.com>
2013-10-23 15:27:28 -0700

    Just mock datetime.datetime.now() in the test generator rather than pushing into the auth class itself.

--- botocore/auth.py
+++ botocore/auth.py
@@ -125,19 +125,15 @@ class SigV4Auth(object):
     Sign a request with Signature V4.
     """
 
-    def __init__(self, credentials, service_name=None, region_name=None,
-                 use_timestamp=None):
+    def __init__(self, credentials, service_name=None, region_name=None):
         self.credentials = credentials
         if self.credentials is None:
             raise NoCredentialsError
         # We initialize these value here so the unit tests can have
         # valid values.  But these will get overriden in ``add_auth``
         # later for real requests.
-        self._use_timestamp = use_timestamp
-        if self._use_timestamp:
-            self.timestamp = self._use_timestamp
-        else:
-            self.timestamp = None
+        now = datetime.datetime.utcnow()
+        self.timestamp = now.strftime('%Y%m%dT%H%M%SZ')
         self.region_name = region_name
         self.service_name = service_name
 
@@ -251,9 +247,8 @@ class SigV4Auth(object):
 
     def add_auth(self, request):
         # Create a new timestamp for each signing event
-        if not self._use_timestamp:
-            now = datetime.datetime.utcnow()
-            self.timestamp = now.strftime('%Y%m%dT%H%M%SZ')
+        now = datetime.datetime.utcnow()
+        self.timestamp = now.strftime('%Y%m%dT%H%M%SZ')
         # This could be a retry.  Make sure the previous
         # authorization header is removed first.
         if 'Authorization' in request.headers:
