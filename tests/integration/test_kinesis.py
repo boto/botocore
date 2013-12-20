b483e98f6e9120271c8f753506edf970e13f7495
James Saryerwinnie <js@jamesls.com>
2013-12-20 15:11:51 -0800

    Fix test import for py26 compat

--- tests/integration/test_kinesis.py
+++ tests/integration/test_kinesis.py
@@ -19,7 +19,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 #
-import unittest
+from tests import unittest
 import itertools
 
 import botocore.session
