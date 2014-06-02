7448e52277d189a39b1a5aff7e0b2dfc8240ef05
James Saryerwinnie <js@jamesls.com>
2014-06-02 11:53:50 -0700

    Initial commit of expect 100-continue support

--- botocore/awsrequest.py
+++ botocore/awsrequest.py
@@ -11,18 +11,150 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-
 import logging
+import select
+import functools
 
 import six
 from botocore.vendored.requests import models
 from botocore.vendored.requests.sessions import REDIRECT_STATI
-
-from botocore.compat import HTTPHeaders, file_type
+from botocore.compat import HTTPHeaders, file_type, HTTPResponse
 from botocore.exceptions import UnseekableStreamError
+from botocore.vendored.requests.packages.urllib3.connection import VerifiedHTTPSConnection
+from botocore.vendored.requests.packages.urllib3.connectionpool import HTTPConnectionPool
+from botocore.vendored.requests.packages.urllib3.connectionpool import HTTPSConnectionPool
 
 
 logger = logging.getLogger(__name__)
+
+
+class AWSHTTPResponse(HTTPResponse):
+    def __init__(self, sock, debuglevel=0, strict=0, method=None,
+                 buffering=False, status_tuple=None):
+        HTTPResponse.__init__(self, sock, debuglevel, strict,
+                                  method, buffering)
+        self._status_tuple = status_tuple
+
+    def _read_status(self):
+        if self._status_tuple is not None:
+            status_tuple = self._status_tuple
+            self._status_tuple = None
+            return status_tuple
+        else:
+            return HTTPResponse._read_status(self)
+
+
+class AWSHTTPConnection(VerifiedHTTPSConnection):
+    """HTTPConnection that supports Expect 100-continue.
+
+    This is conceptually a subclass of httplib.HTTPConnection (though
+    technically we subclass from urllib3, which subclasses
+    httplib.HTTPConnection) and we only override this class to support Expect
+    100-continue, which we need for S3.  As far as I can tell, this is
+    general purpose enough to not be specific to S3, but I'm being
+    tentative and keeping it in botocore because I've only tested
+    this against AWS services.
+
+    """
+    def _send_request(self, method, url, body, headers):
+        # We need to override _send_request to record
+        # whether or not we see an Expect: 100-continue
+        # header via the self._expect_header_set attribute.
+        self._extra = {
+            'method': method,
+            'url': url,
+            'body': body,
+            'headers': headers,
+        }
+        if headers.get('Expect', '') == '100-continue':
+            self._expect_header_set = True
+        else:
+            self._expect_header_set = False
+        return VerifiedHTTPSConnection._send_request(
+            self, method, url, body, headers)
+
+    def _send_output(self, message_body=None):
+        self._buffer.extend((b"", b""))
+        msg = b"\r\n".join(self._buffer)
+        del self._buffer[:]
+        # If msg and message_body are sent in a single send() call,
+        # it will avoid performance problems caused by the interaction
+        # between delayed ack and the Nagle algorithm.
+        if isinstance(message_body, bytes):
+            msg += message_body
+            message_body = None
+        self.send(msg)
+        if self._expect_header_set:
+            # This is our custom behavior.  If the Expect header was
+            # set, it will trigger this custom behavior.
+            logger.debug("Waiting for 100 Continue response.")
+            # Wait for 1 second for the server to send a response.
+            read, write, exc = select.select([self.sock], [], [self.sock], 1)
+            if read:
+                self._handle_expect_response(message_body)
+                return
+            else:
+                # From the RFC:
+                # Because of the presence of older implementations, the
+                # protocol allows ambiguous situations in which a client may
+                # send "Expect: 100-continue" without receiving either a 417
+                # (Expectation Failed) status or a 100 (Continue) status.
+                # Therefore, when a client sends this header field to an origin
+                # server (possibly via a proxy) from which it has never seen a
+                # 100 (Continue) status, the client SHOULD NOT wait for an
+                # indefinite period before sending the request body.
+                logger.debug("No response seen from server, continuing to "
+                             "send the response body.")
+        if message_body is not None:
+            # message_body was not a string (i.e. it is a file), and
+            # we must run the risk of Nagle.
+            self.send(message_body)
+
+    def _handle_expect_response(self, message_body):
+        # This is called when we sent the request headers containing
+        # an Expect: 100-continue header and received a response.
+        # We now need to figure out what to do.
+        fp = self.sock.makefile('rb', 0)
+        maybe_status_line = fp.readline()
+        parts = maybe_status_line.split(None, 2)
+        if self._is_100_continue_status(maybe_status_line):
+            # Read an empty line as per the RFC.
+            fp.readline()
+            logger.debug("100 Continue response seen, now sending request body.")
+            self._send_message_body(message_body)
+            self._expect_header_set = False
+        elif len(parts) == 3 and parts[0].startswith(b'HTTP/'):
+            # From the RFC:
+            # Requirements for HTTP/1.1 origin servers:
+            #
+            # - Upon receiving a request which includes an Expect
+            #   request-header field with the "100-continue"
+            #   expectation, an origin server MUST either respond with
+            #   100 (Continue) status and continue to read from the
+            #   input stream, or respond with a final status code.
+            #
+            # So if we don't get a 100 Continue response, then
+            # whatever the server has sent back is the final response
+            # and don't send the message_body.
+            logger.debug("Received a non 100 Continue response "
+                         "from the server, NOT sending request body.")
+            status_tuple = (parts[0].decode('ascii'),
+                            int(parts[1]), parts[2].decode('ascii'))
+            response_class = functools.partial(
+                AWSHTTPResponse, status_tuple=status_tuple)
+            self.response_class = response_class
+            self._expect_header_set = False
+
+    def _send_message_body(self, message_body):
+        if message_body is not None:
+            self.send(message_body)
+
+    def _is_100_continue_status(self, maybe_status_line):
+        parts = maybe_status_line.split(None, 2)
+        # Check for HTTP/<version> 100 Continue\r\n
+        return (
+            len(parts) == 3 and parts[0].startswith(b'HTTP/') and
+            parts[1] == b'100' and parts[2].startswith(b'Continue'))
 
 
 class AWSRequest(models.RequestEncodingMixin, models.Request):
@@ -36,6 +168,7 @@ class AWSRequest(models.RequestEncodingMixin, models.Request):
         if self.headers is not None:
             for key, value in self.headers.items():
                 headers[key] = value
+        headers['Expect'] = '100-continue'
         self.headers = headers
 
     def prepare(self):
@@ -104,3 +237,6 @@ class AWSPreparedRequest(models.PreparedRequest):
         except Exception as e:
             logger.debug("Unable to rewind stream: %s", e)
             raise UnseekableStreamError(stream_object=self.body)
+
+
+HTTPSConnectionPool.ConnectionCls = AWSHTTPConnection
