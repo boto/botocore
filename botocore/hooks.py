602830a944c95c6774576e074f527bbc59a68d09
James Saryerwinnie <js@jamesls.com>
2014-06-02 15:31:16 -0700

    Add register_first/register_last in hooks module
    Makes it possible to register hooks that you want called
    before/after other hooks.

--- botocore/hooks.py
+++ botocore/hooks.py
@@ -12,11 +12,18 @@
 # language governing permissions and limitations under the License.
 
 import six
+
 import logging
-from collections import defaultdict, deque
+from collections import defaultdict, deque, namedtuple
 from botocore.compat import accepts_kwargs
 
 logger = logging.getLogger(__name__)
+
+
+NodeList = namedtuple('NodeList', ['first', 'middle', 'last'])
+_FIRST = 0
+_MIDDLE = 1
+_LAST = 2
 
 
 def first_non_none_response(responses, default=None):
@@ -49,16 +56,81 @@ def first_non_none_response(responses, default=None):
 
 class BaseEventHooks(object):
     def emit(self, event_name, **kwargs):
+        """Call all handlers subscribed to an event.
+
+        :type event_name: str
+        :param event_name: The name of the event to emit.
+
+        :type **kwargs: dict
+        :param **kwargs: Arbitrary kwargs to pass through to the
+            subscribed handlers.  The ``event_name`` will be injected
+            into the kwargs so it's not necesary to add this to **kwargs.
+
+        :rtype: list of tuples
+        :return: A list of ``(handler_func, handler_func_return_value)``
+
+        """
         return []
 
     def register(self, event_name, handler, unique_id=None,
                  unique_id_uses_count=False):
+        """Register an event handler for a given event.
+
+        If a ``unique_id`` is given, the handler will not be registered
+        if a handler with the ``unique_id`` has already been registered.
+
+        Handlers are called in the order they have been registered.
+        Note handlers can also be registered with ``register_first()``
+        and ``register_last()``.  All handlers registered with
+        ``register_first()`` are called before handlers registered
+        with ``register()`` which are called before handlers registered
+        with ``register_last()``.
+
+        """
+        self._verify_and_register(event_name, handler, unique_id,
+                                  register_method=self._register,
+                                  unique_id_uses_count=unique_id_uses_count)
+
+    def register_first(self, event_name, handler, unique_id=None,
+                       unique_id_uses_count=False):
+        """Register an event handler to be called first for an event.
+
+        All event handlers registered with ``register_first()`` will
+        be called before handlers registered with ``register()`` and
+        ``register_last()``.
+
+        """
+        self._verify_and_register(event_name, handler, unique_id,
+                                  register_method=self._register_first,
+                                  unique_id_uses_count=unique_id_uses_count)
+
+    def register_last(self, event_name, handler, unique_id=None,
+                      unique_id_uses_count=False):
+        """Register an event handler to be called last for an event.
+
+        All event handlers registered with ``register_last()`` will be called
+        after handlers registered with ``register_first()`` and ``register()``.
+
+        """
+        self._verify_and_register(event_name, handler, unique_id,
+                                  register_method=self._register_last,
+                                  unique_id_uses_count=unique_id_uses_count)
+
+    def _verify_and_register(self, event_name, handler, unique_id,
+                             register_method, unique_id_uses_count):
         self._verify_is_callable(handler)
         self._verify_accept_kwargs(handler)
-        self._register(event_name, handler, unique_id, unique_id_uses_count)
+        register_method(event_name, handler, unique_id, unique_id_uses_count)
 
     def unregister(self, event_name, handler=None, unique_id=None,
                    unique_id_uses_count=False):
+        """Unregister an event handler for a given event.
+
+        If no ``unique_id`` was given during registration, then the
+        first instance of the event handler is removed (if the event
+        handler has been registered multiple times).
+
+        """
         pass
 
     def _verify_is_callable(self, func):
@@ -80,45 +152,6 @@ class BaseEventHooks(object):
                                  "arguments (**kwargs)" % func)
         except TypeError:
             return False
-
-
-class EventHooks(BaseEventHooks):
-    def __init__(self):
-        # event_name -> [handler, ...]
-        self._handlers = defaultdict(list)
-
-    def emit(self, event_name, **kwargs):
-        """Call all handlers subscribed to an event.
-
-        :type event_name: str
-        :param event_name: The name of the event to emit.
-
-        :type **kwargs: dict
-        :param **kwargs: Arbitrary kwargs to pass through to the
-            subscribed handlers.  The ``event_name`` will be injected
-            into the kwargs so it's not necesary to add this to **kwargs.
-
-        :rtype: list of tuples
-        :return: A list of ``(handler_func, handler_func_return_value)``
-
-        """
-        kwargs['event_name'] = event_name
-        responses = []
-        for handler in self._handlers[event_name]:
-            response = handler(**kwargs)
-            responses.append((handler, response))
-        return responses
-
-    def _register(self, event_name, handler, unique_id=None,
-                  unique_id_uses_count=False):
-        self._handlers[event_name].append(handler)
-
-    def unregister(self, event_name, handler, unique_id=None,
-                   unique_id_uses_count=False):
-        try:
-            self._handlers[event_name].remove(handler)
-        except ValueError:
-            pass
 
 
 class HierarchicalEmitter(BaseEventHooks):
@@ -155,6 +188,22 @@ class HierarchicalEmitter(BaseEventHooks):
 
     def _register(self, event_name, handler, unique_id=None,
                   unique_id_uses_count=False):
+        self._register_section(event_name, handler, unique_id,
+                               unique_id_uses_count, section=_MIDDLE)
+
+    def _register_first(self, event_name, handler, unique_id=None,
+                        unique_id_uses_count=False):
+        self._register_section(event_name, handler, unique_id,
+                               unique_id_uses_count, section=_FIRST)
+
+    def _register_last(self, event_name, handler, unique_id,
+                       unique_id_uses_count=False):
+        self._register_section(event_name, handler, unique_id,
+                               unique_id_uses_count, section=_LAST)
+
+
+    def _register_section(self, event_name, handler, unique_id,
+                          unique_id_uses_count, section):
         if unique_id is not None:
             if unique_id in self._unique_id_cache:
                 # We've already registered a handler using this unique_id
@@ -180,13 +229,14 @@ class HierarchicalEmitter(BaseEventHooks):
                 # Note that the trie knows nothing about the unique
                 # id.  We track uniqueness in this class via the
                 # _unique_id_cache.
-                self._handlers.append_item(event_name, handler)
+                self._handlers.append_item(event_name, handler,
+                                           section=section)
                 unique_id_cache_item = {'handler': handler}
                 if unique_id_uses_count:
                     unique_id_cache_item['count'] = 1
                 self._unique_id_cache[unique_id] = unique_id_cache_item
         else:
-            self._handlers.append_item(event_name, handler)
+            self._handlers.append_item(event_name, handler, section=section)
         # Super simple caching strategy for now, if we change the registrations
         # clear the cache.  This has the opportunity for smarter invalidations.
         self._lookup_cache = {}
@@ -256,7 +306,7 @@ class _PrefixTrie(object):
         # {'foo': {'children': {'bar': {...}}}}.
         self._root = {'chunk': None, 'children': {}, 'values': None}
 
-    def append_item(self, key, value):
+    def append_item(self, key, value, section=_MIDDLE):
         """Add an item to a key.
 
         If a value is already associated with that key, the new
@@ -272,9 +322,8 @@ class _PrefixTrie(object):
             else:
                 current = current['children'][part]
         if current['values'] is None:
-            current['values'] = [value]
-        else:
-            current['values'].append(value)
+            current['values'] = NodeList([], [], [])
+        current['values'][section].append(value)
 
     def prefix_search(self, key):
         """Collect all items that are prefixes of key.
@@ -300,12 +349,17 @@ class _PrefixTrie(object):
         while stack:
             current_node, index = stack.pop()
             if current_node['values']:
-                seq = reversed(current_node['values'])
                 # We're using extendleft because we want
                 # the values associated with the node furthest
                 # from the root to come before nodes closer
-                # to the root.
-                collected.extendleft(seq)
+                # to the root.  extendleft() also adds its items
+                # in right-left order so .extendleft([1, 2, 3])
+                # will result in final_list = [3, 2, 1], which is
+                # why we reverse the lists.
+                node_list = current_node['values']
+                complete_order = (node_list.first + node_list.middle +
+                                  node_list.last)
+                collected.extendleft(reversed(complete_order))
             if not index == key_parts_len:
                 children = current_node['children']
                 directs = children.get(key_parts[index])
@@ -336,7 +390,13 @@ class _PrefixTrie(object):
             if next_node is not None:
                 self._remove_item(next_node, key_parts, value, index + 1)
                 if index == len(key_parts) - 1:
-                    next_node['values'].remove(value)
+                    node_list = next_node['values']
+                    if value in node_list.first:
+                        node_list.first.remove(value)
+                    elif value in node_list.middle:
+                        node_list.middle.remove(value)
+                    elif value in node_list.last:
+                        node_list.last.remove(value)
                 if not next_node['children'] and not next_node['values']:
                     # Then this is a leaf node with no values so
                     # we can just delete this link from the parent node.
