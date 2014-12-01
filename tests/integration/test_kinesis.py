4cc7aed08903f43a2fdf5f2db1565c25416fba03
James Saryerwinnie <js@jamesls.com>
2014-12-01 13:52:49 -0800

    Add integ test for blob serialization in json protocol
    This also included adding a waiter config for kinesis.

--- tests/integration/test_kinesis.py
+++ tests/integration/test_kinesis.py
@@ -10,6 +10,8 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+import time
+import random
 from tests import unittest
 
 import botocore.session
@@ -19,10 +21,36 @@ class TestKinesisListStreams(unittest.TestCase):
     def setUp(self):
         self.session = botocore.session.get_session()
         self.client = self.session.create_client('kinesis', 'us-east-1')
+        self.stream_name = 'botocore-test-%s-%s' % (int(time.time()),
+                                                    random.randint(1, 100))
 
     def test_list_streams(self):
         parsed = self.client.list_streams()
         self.assertIn('StreamNames', parsed)
+
+    def test_can_put_stream_blob(self):
+        self.client.create_stream(StreamName=self.stream_name,
+                                  ShardCount=1)
+        waiter = self.client.get_waiter('stream_exists')
+        waiter.wait(StreamName=self.stream_name)
+        self.addCleanup(self.client.delete_stream,
+                        StreamName=self.stream_name)
+
+        self.client.put_record(
+            StreamName=self.stream_name, PartitionKey='foo', Data='foobar')
+        # Give it a few seconds for the record to get into the stream.
+        time.sleep(10)
+
+        stream = self.client.describe_stream(StreamName=self.stream_name)
+        shard = stream['StreamDescription']['Shards'][0]
+        shard_iterator = self.client.get_shard_iterator(
+            StreamName=self.stream_name, ShardId=shard['ShardId'],
+            ShardIteratorType='TRIM_HORIZON')
+
+        records = self.client.get_records(
+            ShardIterator=shard_iterator['ShardIterator'])
+        self.assertTrue(len(records['Records']) > 0)
+        self.assertEqual(records['Records'][0]['Data'], 'foobar')
 
 
 if __name__ == '__main__':
