7b7a11d54ffc7c8ea69aedc3ef42d5937e0f7f21
Daniel G. Taylor <danielgtaylor@gmail.com>
2014-12-19 15:28:37 -0800

    Add RequestSigner class.
    This introduces a new `RequestSigner` class to handle the signing of
    requests. It fires the following events:
    
    * `choose-signer.service.operation`: Allow overriding signature version
    * `before-sign.service.operation`: Allow mutating request before signing
    
    It is currently not used by anything, but is included as part of #414.

---
+++ botocore/signers.py
@@ -0,0 +1,137 @@
+# Copyright 2014 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+import botocore.auth
+
+from botocore.exceptions import UnknownSignatureVersionError
+
+class RequestSigner(object):
+    """
+    An object to sign requests before they go out over the wire using
+    one of the authentication mechanisms defined in ``auth.py``. This
+    class fires two events scoped to a service and operation name:
+
+    * choose-signer: Allows overriding the auth signer name.
+    * before-sign: Allows mutating the request before signing.
+
+    Together these events allow for customization of the request
+    signing pipeline, including overrides, request path manipulation,
+    and disabling signing per operation.
+
+    :type service_name: string
+    :param service_name: Name of the service, e.g. ``S3``
+    :type region_name: string
+    :param region_name: Name of the service region, e.g. ``us-east-1``
+    :type signing_name: string
+    :param signing_name: Service signing name. This is usually the
+                         same as the service name, but can differ. E.g.
+                         ``emr`` vs. ``elasticmapreduce``.
+    :type signature_version: string
+    :param signature_version: Signature name like ``v4``.
+    :type credentials: :py:class:`~botocore.credentials.Credentials`
+    :param credentials: User credentials with which to sign requests.
+    :type event_emitter: :py:class:`~botocore.hooks.BaseEventHooks`
+    :param event_emitter: Extension mechanism to fire events.
+    :type scoped_config: dict
+    :param scoped_config: User configuration which may include
+                          signature overrides.
+    """
+    def __init__(self, service_name, region_name, signing_name,
+                 signature_version, credentials, event_emitter,
+                 scoped_config):
+        self._service_name = service_name
+        self._region_name = region_name
+        self._signing_name = signing_name
+        self._signature_version = signature_version
+        self._credentials = credentials
+        self._event_emitter = event_emitter
+        self._scoped_config = scoped_config
+
+        # Used to cache auth instances since one request signer
+        # can be used for many requests in a single client.
+        self._cache = {}
+
+    def sign(self, operation_name, request):
+        """
+        Sign a request before it goes out over the wire.
+
+        :type operation_name: string
+        :param operation_name: The name of the current operation, e.g.
+                               ``ListBuckets``.
+        :type request: AWSRequest
+        :param request: The request object to be sent over the wire.
+        """
+        signature_version = self._signature_version
+
+        # Allow overriding signature version. A response of a blank
+        # string means no signing is performed. A response of ``None``
+        # means that the default signing method is used.
+        handler, response = self._event_emitter.emit_until_response(
+            'choose-signer.{0}.{1}'.format(self._service_name, operation_name),
+            signing_name=self._signing_name, region_name=self._region_name,
+            signature_version=signature_version, config=self._scoped_config)
+        if response is not None:
+            signature_version = response
+
+        # Allow mutating request before signing
+        self._event_emitter.emit(
+            'before-sign.{0}.{1}'.format(self._service_name, operation_name),
+            request=request, signing_name=self._signing_name,
+            region_name=self._region_name,
+            signature_version=signature_version, request_signer=self)
+
+        # Sign the request if the signature version isn't None or blank
+        if signature_version:
+            signer = self.get_auth(self._signing_name, self._region_name,
+                                    signature_version)
+            signer.add_auth(request=request)
+
+    def get_auth(self, signing_name, region_name, signature_version=None):
+        """
+        Get an auth instance which can be used to sign a request
+        using the given signature version.
+
+        :type signing_name: string
+        :param signing_name: Service signing name. This is usually the
+                             same as the service name, but can differ. E.g.
+                             ``emr`` vs. ``elasticmapreduce``.
+        :type region_name: string
+        :param region_name: Name of the service region, e.g. ``us-east-1``
+        :type signature_version: string
+        :param signature_version: Signature name like ``v4``.
+        :rtype: :py:class:`~botocore.auth.BaseSigner`
+        :return: Auth instance to sign a request.
+        """
+        if signature_version is None:
+            signature_version = self._signature_version
+
+        key = '{0}.{1}.{2}'.format(signature_version, region_name,
+                                   signing_name)
+        if key in self._cache:
+            return self._cache[key]
+
+        cls = botocore.auth.AUTH_TYPE_MAPS.get(signature_version)
+        if cls is None:
+            raise UnknownSignatureVersionError(
+                signature_version=signature_version)
+        else:
+            kwargs = {'credentials': self._credentials}
+            if cls.REQUIRES_REGION:
+                if self._region_name is None:
+                    raise botocore.exceptions.NoRegionError(
+                        env_var='AWS_DEFAULT_REGION')
+                kwargs['region_name'] = region_name
+                kwargs['service_name'] = signing_name
+            auth = cls(**kwargs)
+            self._cache[key] = auth
+            return auth
---
+++ tests/unit/test_signers.py
@@ -0,0 +1,128 @@
+# Copyright 2012-2014 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+import mock
+
+import botocore.auth
+
+from botocore.credentials import Credentials
+from botocore.exceptions import NoRegionError, UnknownSignatureVersionError
+from botocore.signers import RequestSigner
+
+from tests import unittest
+
+
+class TestSigner(unittest.TestCase):
+    def setUp(self):
+        self.credentials = Credentials('key', 'secret')
+        self.emitter = mock.Mock()
+        self.emitter.emit_until_response.return_value = (None, None)
+        self.config = {}
+        self.signer = RequestSigner(
+            'service_name', 'region_name', 'signing_name',
+            'v4', self.credentials, self.emitter, self.config)
+
+    def test_region_required_for_sigv4(self):
+        self.signer = RequestSigner(
+            'service_name', None, 'signing_name', 'v4', self.credentials,
+            self.emitter, self.config)
+
+        with self.assertRaises(NoRegionError):
+            self.signer.sign('operation_name', mock.Mock())
+
+    def test_get_auth(self):
+        auth_cls = mock.Mock()
+        with mock.patch.dict(botocore.auth.AUTH_TYPE_MAPS,
+                             {'v4': auth_cls}):
+            auth = self.signer.get_auth('service_name', 'region_name')
+
+            self.assertEqual(auth, auth_cls.return_value)
+            auth_cls.assert_called_with(
+                credentials=self.credentials, service_name='service_name',
+                region_name='region_name')
+
+    def test_get_auth_cached(self):
+        auth_cls = mock.Mock()
+        with mock.patch.dict(botocore.auth.AUTH_TYPE_MAPS,
+                             {'v4': auth_cls}):
+            auth1 = self.signer.get_auth('service_name', 'region_name')
+            auth2 = self.signer.get_auth('service_name', 'region_name')
+
+        self.assertEqual(auth1, auth2)
+
+    def test_get_auth_signature_override(self):
+        auth_cls = mock.Mock()
+        with mock.patch.dict(botocore.auth.AUTH_TYPE_MAPS,
+                             {'v4-custom': auth_cls}):
+            auth = self.signer.get_auth(
+                'service_name', 'region_name', signature_version='v4-custom')
+
+            self.assertEqual(auth, auth_cls.return_value)
+            auth_cls.assert_called_with(
+                credentials=self.credentials, service_name='service_name',
+                region_name='region_name')
+
+    def test_get_auth_bad_override(self):
+        with self.assertRaises(UnknownSignatureVersionError):
+            self.signer.get_auth('service_name', 'region_name',
+                                 signature_version='bad')
+
+    def test_emits_choose_signer(self):
+        request = mock.Mock()
+
+        with mock.patch.dict(botocore.auth.AUTH_TYPE_MAPS,
+                             {'v4': mock.Mock()}):
+            self.signer.sign('operation_name', request)
+
+        self.emitter.emit_until_response.assert_called_with(
+            'choose-signer.service_name.operation_name',
+            signing_name='signing_name', region_name='region_name',
+            signature_version='v4', config=self.config)
+
+    def test_choose_signer_override(self):
+        request = mock.Mock()
+        auth = mock.Mock()
+        auth.REQUIRES_REGION = False
+        self.emitter.emit_until_response.return_value = (None, 'custom')
+
+        with mock.patch.dict(botocore.auth.AUTH_TYPE_MAPS,
+                             {'custom': auth}):
+            self.signer.sign('operation_name', request)
+
+        auth.assert_called_with(credentials=self.credentials)
+        auth.return_value.add_auth.assert_called_with(request=request)
+
+    def test_emits_before_sign(self):
+        request = mock.Mock()
+
+        with mock.patch.dict(botocore.auth.AUTH_TYPE_MAPS,
+                             {'v4': mock.Mock()}):
+            self.signer.sign('operation_name', request)
+
+        self.emitter.emit.assert_called_with(
+            'before-sign.service_name.operation_name',
+            request=mock.ANY, signing_name='signing_name',
+            region_name='region_name', signature_version='v4',
+            request_signer=self.signer)
+
+    def test_disable_signing(self):
+        # Returning a blank string from choose-signer disabled signing!
+        request = mock.Mock()
+        auth = mock.Mock()
+        self.emitter.emit_until_response.return_value = (None, '')
+
+        with mock.patch.dict(botocore.auth.AUTH_TYPE_MAPS,
+                             {'v4': auth}):
+            self.signer.sign('operation_name', request)
+
+        auth.assert_not_called()
