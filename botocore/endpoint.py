8d578490deff4999981342ba5aec8748697e24c7
kyleknap <kyleknap@amazon.com>
2015-03-25 15:44:08 -0700

    Remove the user agent from endpoint
    In addition, all of the logic used to build the request dict was moved to
    the client. So the endpoint only takes in a prepared request dict, does a
    simple translation to a response object, and send/receive the request over
    the wire.

--- botocore/endpoint.py
+++ botocore/endpoint.py
@@ -22,12 +22,10 @@ from botocore.vendored.requests.utils import get_environ_proxies
 from botocore.vendored.requests.exceptions import ConnectionError
 from botocore.vendored import six
 
+from botocore.awsrequest import create_request_object
 from botocore.exceptions import BaseEndpointResolverError
 from botocore.exceptions import EndpointConnectionError
-from botocore.awsrequest import AWSRequest
-from botocore.compat import filter_ssl_warnings, urlsplit
-from botocore.compat import urlunsplit
-from botocore.utils import percent_encode_sequence
+from botocore.compat import filter_ssl_san_warnings
 from botocore.utils import is_valid_endpoint_url
 from botocore.hooks import first_non_none_response
 from botocore.response import StreamingBody
@@ -75,64 +73,6 @@ class PreserveAuthSession(Session):
         pass
 
 
-class RequestCreator(object):
-    """This object can convert a request dict to an AWSRequest class."""
-
-    def create_request_object(self, request_dict, user_agent, endpoint_url):
-        """
-
-        :type request_dict: dict
-        :param request_dict:  The request dict (created from the ``serialize``
-            module).
-
-        :type user_agent: string
-        :param user_agent: The user agent to use for this request.
-
-        :type endpoint_url: string
-        :param endpoint_url: The full endpoint url, which contains at least
-            the scheme, the hostname, and optionally any path components.
-
-        :rtype: ``botocore.awsrequest.AWSRequest``
-        :return: An AWSRequest object based on the request_dict.
-
-        """
-        r = request_dict
-        headers = r['headers']
-        headers['User-Agent'] = user_agent
-        url = self._urljoin(endpoint_url, r['url_path'])
-        if r['query_string']:
-            encoded_query_string = percent_encode_sequence(r['query_string'])
-            if '?' not in url:
-                url += '?%s' % encoded_query_string
-            else:
-                url += '&%s' % encoded_query_string
-        request = AWSRequest(method=r['method'], url=url,
-                             data=r['body'],
-                             headers=headers)
-        return request
-
-    def _urljoin(self, endpoint_url, url_path):
-        p = urlsplit(endpoint_url)
-        # <part>   - <index>
-        # scheme   - p[0]
-        # netloc   - p[1]
-        # path     - p[2]
-        # query    - p[3]
-        # fragment - p[4]
-        if not url_path or url_path == '/':
-            # If there's no path component, ensure the URL ends with
-            # a '/' for backwards compatibility.
-            if not p[2]:
-                return endpoint_url + '/'
-            return endpoint_url
-        if p[2].endswith('/') and url_path.startswith('/'):
-            new_path = p[2][:-1] + url_path
-        else:
-            new_path = p[2] + url_path
-        reconstructed = urlunsplit((p[0], p[1], new_path, p[3], p[4]))
-        return reconstructed
-
-
 class Endpoint(object):
     """
     Represents an endpoint for a particular service in a specific
@@ -144,12 +84,11 @@ class Endpoint(object):
     :ivar session: The session object.
     """
 
-    def __init__(self, host, user_agent, endpoint_prefix,
+    def __init__(self, host, endpoint_prefix,
                  event_emitter, proxies=None, verify=True,
                  timeout=DEFAULT_TIMEOUT, response_parser_factory=None):
         self._endpoint_prefix = endpoint_prefix
         self._event_emitter = event_emitter
-        self._user_agent = user_agent
         self.host = host
         self.verify = verify
         if proxies is None:
@@ -161,7 +100,6 @@ class Endpoint(object):
         if response_parser_factory is None:
             response_parser_factory = parsers.ResponseParserFactory()
         self._response_parser_factory = response_parser_factory
-        self._request_creator = RequestCreator()
 
     def __repr__(self):
         return '%s(%s)' % (self._endpoint_prefix, self.host)
@@ -172,8 +110,7 @@ class Endpoint(object):
         return self._send_request(request_dict, operation_model)
 
     def create_request(self, params, operation_model=None):
-        request = self._request_creator.create_request_object(
-            params, self._user_agent, self.host)
+        request = create_request_object(params)
         if operation_model:
             event_name = 'request-created.{endpoint_prefix}.{op_name}'.format(
                 endpoint_prefix=self._endpoint_prefix,
@@ -277,12 +214,10 @@ class Endpoint(object):
 
 
 class EndpointCreator(object):
-    def __init__(self, endpoint_resolver, configured_region, event_emitter,
-                 user_agent):
+    def __init__(self, endpoint_resolver, configured_region, event_emitter):
         self._endpoint_resolver = endpoint_resolver
         self._configured_region = configured_region
         self._event_emitter = event_emitter
-        self._user_agent = user_agent
 
     def create_endpoint(self, service_model, region_name=None, is_secure=True,
                         endpoint_url=None, verify=None,
@@ -319,9 +254,8 @@ class EndpointCreator(object):
                       verify, response_parser_factory):
         endpoint_prefix = service_model.endpoint_prefix
         event_emitter = self._event_emitter
-        user_agent = self._user_agent
         return self._get_endpoint_complex(endpoint_prefix, endpoint_url,
-                                          verify, user_agent, event_emitter,
+                                          verify, event_emitter,
                                           response_parser_factory)
 
     def _get_proxies(self, url):
@@ -344,13 +278,12 @@ class EndpointCreator(object):
 
     def _get_endpoint_complex(self, endpoint_prefix,
                               endpoint_url, verify,
-                              user_agent, event_emitter,
+                              event_emitter,
                               response_parser_factory=None):
         proxies = self._get_proxies(endpoint_url)
         verify = self._get_verify_value(verify)
         return Endpoint(
             endpoint_url,
-            user_agent=user_agent,
             endpoint_prefix=endpoint_prefix,
             event_emitter=event_emitter,
             proxies=proxies,
