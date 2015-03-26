f500016b89f9ede677ea1865b24e8b29d590ce69
kyleknap <kyleknap@amazon.com>
2015-03-26 12:47:59 -0700

    Create a generate_url method on the signer class
    Allows users to presign a url. Converted existing customizations and
    tests to use the presigner as well.

--- tests/integration/test_s3.py
+++ tests/integration/test_s3.py
@@ -31,6 +31,8 @@ from nose.plugins.attrib import attr
 from botocore.vendored.requests import adapters
 from botocore.vendored.requests.exceptions import ConnectionError
 from botocore.compat import six
+from botocore.awsrequest import prepare_request_dict
+from botocore.signers import RequestSigner
 import botocore.session
 import botocore.auth
 import botocore.credentials
@@ -126,11 +128,12 @@ class BaseS3ClientTest(unittest.TestCase):
 class TestS3BaseWithBucket(BaseS3ClientTest):
     def setUp(self):
         super(TestS3BaseWithBucket, self).setUp()
-        self.region = 'us-west-2'
         self.bucket_name = self.create_bucket()
 
 
 class TestS3Buckets(TestS3BaseWithBucket):
+    def setUp(self):
+        super(TestS3Buckets, self).setUp()
 
     def test_can_make_request(self):
         # Basic smoke test to ensure we can talk to s3.
@@ -144,7 +147,7 @@ class TestS3Buckets(TestS3BaseWithBucket):
         result = self.client.get_bucket_location(Bucket=self.bucket_name)
         self.assertIn('LocationConstraint', result)
         # For buckets in us-east-1 (US Classic Region) this will be None
-        self.assertEqual(result['LocationConstraint'], self.region)
+        self.assertEqual(result['LocationConstraint'], None)
 
 
 class TestS3Objects(TestS3BaseWithBucket):
@@ -418,62 +421,78 @@ class TestS3Copy(TestS3BaseWithBucket):
         self.assert_status_code(parsed, 200)
 
 
-# TODO: Convert this test to the presigning function that is to be
-# added later
-@unittest.skip('Need a method for presigning')
-class TestS3Presign(BaseS3ClientTest):
-    def setUp(self):
-        super(TestS3Presign, self).setUp()
-        self.bucket_name = 'botocoretest%s-%s' % (
-            int(time.time()), random.randint(1, 1000))
-
-        operation = self.service.get_operation('CreateBucket')
-        response = operation.call(self.endpoint, bucket=self.bucket_name)
-        self.assertEqual(response[0].status_code, 200)
+class BaseS3PresignTest(BaseS3ClientTest):
 
     def tearDown(self):
         for key in self.keys:
-            operation = self.service.get_operation('DeleteObject')
-            operation.call(self.endpoint, bucket=self.bucket_name,
-                           key=key)
-        self.delete_bucket(self.bucket_name)
-        super(TestS3Presign, self).tearDown()
+            self.client.delete_object(
+                Bucket=self.bucket_name, Key=key)
+        super(BaseS3PresignTest, self).tearDown()
 
-    def test_can_retrieve_presigned_object(self):
-        key_name = 'mykey'
-        self.create_object(key_name=key_name, body='foobar')
-        signer = botocore.auth.S3SigV4QueryAuth(
-            credentials=self.service.session.get_credentials(),
-            region_name='us-east-1', service_name='s3', expires=60)
-        op = self.service.get_operation('GetObject')
-        params = op.build_parameters(bucket=self.bucket_name, key=key_name)
-        request = self.endpoint.create_request(params)
-        signer.add_auth(request.original)
-        presigned_url = request.original.prepare().url
-        # We should now be able to retrieve the contents of 'mykey' using
-        # this presigned url.
-        self.assertEqual(requests.get(presigned_url).content, b'foobar')
+    def setup_presigner(self):
+        self.key = 'myobject'
+        self.bucket_name = self.create_bucket()
+        self.create_object(key_name=self.key)
+        self.emitter = self.session.get_component('event_emitter')
+        self.s3_model = self.session.get_service_model('s3')
+        self.operation_model = self.s3_model.operation_model('GetObject')
+        self.serializer = botocore.serialize.create_serializer(
+            'rest-xml', include_validation=True)
+        self.params = {'Bucket': self.bucket_name, 'Key': self.key}
+        self.request_dict = self.serializer.serialize_to_request(
+            self.params, self.operation_model)
 
 
-@unittest.skip('Need a method for presigning')
-class TestS3PresignFixHost(BaseS3ClientTest):
-    def test_presign_does_not_change_host(self):
-        endpoint = self.service.get_endpoint('us-west-2')
-        key_name = 'mykey'
-        bucket_name = 'mybucket'
-        signer = botocore.auth.S3SigV4QueryAuth(
-            credentials=self.session.get_credentials(),
-            region_name='us-west-2', service_name='s3', expires=60)
-        op = self.service.get_operation('GetObject')
-        params = op.build_parameters(bucket=bucket_name, key=key_name)
-        request = endpoint.create_request(params)
-        signer.add_auth(request.original)
-        presigned_url = request.original.prepare().url
-        # We should not have rewritten the host to be s3.amazonaws.com.
-        self.assertTrue(presigned_url.startswith(
-            'https://s3-us-west-2.amazonaws.com/mybucket/mykey'),
+class TestS3PresignUsStandard(BaseS3PresignTest):
+    def setUp(self):
+        super(TestS3PresignUsStandard, self).setUp()
+        self.region = 'us-east-1'
+        self.client = self.session.create_client('s3', self.region)
+        self.setup_presigner()
+
+    def test_presign_sigv4(self):
+        creds = self.session.get_credentials()
+        signer = RequestSigner(
+            's3', 'us-east-1', 's3', 's3v4', creds, self.emitter)
+        prepare_request_dict(
+            self.request_dict, user_agent=self.session.user_agent(),
+            endpoint_url=self.client.meta.endpoint_url)
+        presigned_url = signer.generate_url(self.request_dict)
+        self.assertTrue(
+            presigned_url.startswith(
+                'https://s3.amazonaws.com/%s/%s' % (
+                    self.bucket_name, self.key)),
+            "Host was suppose to be the us-east-1 endpoint, instead "
+            "got: %s" % presigned_url)
+        # Now try to retrieve the object using the presigned url.
+        self.assertEqual(requests.get(presigned_url).content, b'foo')
+
+
+class TestS3PresignNonUsStandard(BaseS3PresignTest):
+
+    def setUp(self):
+        super(TestS3PresignNonUsStandard, self).setUp()
+        self.region = 'us-west-2'
+        self.client = self.session.create_client('s3', self.region)
+        self.setup_presigner()
+
+    def test_presign_sigv4(self):
+        creds = self.session.get_credentials()
+        signer = RequestSigner(
+            's3', 'us-west-2', 's3', 's3v4', creds, self.emitter)
+        prepare_request_dict(
+            self.request_dict, user_agent=self.session.user_agent(),
+            endpoint_url=self.client.meta.endpoint_url)
+        presigned_url = signer.generate_url(self.request_dict)
+
+        self.assertTrue(
+            presigned_url.startswith(
+                'https://s3-us-west-2.amazonaws.com/%s/%s' % (
+                    self.bucket_name, self.key)),
             "Host was suppose to be the us-west-2 endpoint, instead "
             "got: %s" % presigned_url)
+        # Now try to retrieve the object using the presigned url.
+        self.assertEqual(requests.get(presigned_url).content, b'foo')
 
 
 class TestCreateBucketInOtherRegion(TestS3BaseWithBucket):
