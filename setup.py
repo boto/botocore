e547a97892837fa7ff06b31af2076bbf7bdf117d
James Saryerwinnie <js@jamesls.com>
2015-06-08 13:03:09 -0700

    Use extras_requires for conditional dependencies
    So we don't have different code paths for whl files
    and .tar.gz files.

--- setup.py
+++ setup.py
@@ -10,19 +10,6 @@ requires = ['jmespath==0.7.1',
             'python-dateutil>=2.1,<3.0.0']
 
 
-if sys.version_info[:2] == (2, 6):
-    # For python2.6 we have a few other dependencies.
-    # First we need an ordered dictionary so we use the
-    # 2.6 backport.
-    requires.append('ordereddict==1.1')
-    # Then we need simplejson.  This is because we need
-    # a json version that allows us to specify we want to
-    # use an ordereddict instead of a normal dict for the
-    # JSON objects.  The 2.7 json module has this.  For 2.6
-    # we need simplejson.
-    requires.append('simplejson==3.3.0')
-
-
 setup(
     name='botocore',
     version=botocore.__version__,
@@ -38,6 +25,12 @@ setup(
     package_dir={'botocore': 'botocore'},
     include_package_data=True,
     install_requires=requires,
+    extras_require={
+        ':python_version=="2.6"': [
+            'ordereddict==1.1',
+            'simplejson==3.3.0',
+        ]
+    },
     license=open("LICENSE.txt").read(),
     classifiers=(
         'Development Status :: 4 - Beta',
