eb71ff40a88f9b600089ae41cfd2ec7c8466ce7d
James Saryerwinnie <js@jamesls.com>
2015-06-11 22:59:07 -0700

    Fix regression in older versions of pip with cond deps

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import botocore
+import sys
 
 from setuptools import setup, find_packages
 
@@ -7,6 +8,19 @@ from setuptools import setup, find_packages
 requires = ['jmespath==0.7.1',
             'bcdoc>=0.16.0,<0.17.0',
             'python-dateutil>=2.1,<3.0.0']
+
+
+if sys.version_info[:2] == (2, 6):
+    # For python2.6 we have a few other dependencies.
+    # First we need an ordered dictionary so we use the
+    # 2.6 backport.
+    requires.append('ordereddict==1.1')
+    # Then we need simplejson.  This is because we need
+    # a json version that allows us to specify we want to
+    # use an ordereddict instead of a normal dict for the
+    # JSON objects.  The 2.7 json module has this.  For 2.6
+    # we need simplejson.
+    requires.append('simplejson==3.3.0')
 
 
 setup(
