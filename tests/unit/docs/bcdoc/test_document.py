89ed65c9e1f54fe4f10bab8fcdbd98f142ec3229
kyleknap <kyleknap@amazon.com>
2015-06-17 12:30:05 -0700

    Copy bcdoc library into botocore
    This was just a straight copy. No other modifications on imports were made.

---
+++ tests/unit/docs/bcdoc/test_document.py
@@ -0,0 +1,134 @@
+# Copyright (c) 2012-2013 Mitch Garnaat http://garnaat.org/
+# Copyright 2012-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+import six
+
+from tests import unittest
+from bcdoc.restdoc import ReSTDocument, DocumentStructure
+
+
+class TestReSTDocument(unittest.TestCase):
+
+    def test_write(self):
+        doc = ReSTDocument()
+        doc.write('foo')
+        self.assertEqual(doc.getvalue(), six.b('foo'))
+
+    def test_writeln(self):
+        doc = ReSTDocument()
+        doc.writeln('foo')
+        self.assertEqual(doc.getvalue(), six.b('foo\n'))
+
+    def test_include_doc_string(self):
+        doc = ReSTDocument()
+        doc.include_doc_string('<p>this is a <code>test</code></p>')
+        self.assertEqual(doc.getvalue(), six.b('\n\nthis is a ``test`` \n\n'))
+        
+    def test_remove_doc_string(self):
+        doc = ReSTDocument()
+        doc.writeln('foo')
+        doc.include_doc_string('<p>this is a <code>test</code></p>')
+        doc.remove_last_doc_string()
+        self.assertEqual(doc.getvalue(), six.b('foo\n'))
+
+
+class TestDocumentStructure(unittest.TestCase):
+    def setUp(self):
+        self.name = 'mydoc'
+        self.doc_structure = DocumentStructure(self.name)
+
+    def test_name(self):
+        self.assertEqual(self.doc_structure.name, self.name)
+
+    def test_path(self):
+        self.assertEqual(self.doc_structure.path, [self.name])
+        self.doc_structure.path = ['foo']
+        self.assertEqual(self.doc_structure.path, ['foo'])
+
+    def test_add_new_section(self):
+        section = self.doc_structure.add_new_section('mysection')
+
+        # Ensure the name of the section is correct
+        self.assertEqual(section.name, 'mysection')
+
+        # Ensure we can get the section.
+        self.assertEqual(
+            self.doc_structure.get_section('mysection'), section)
+
+        # Ensure the path is correct
+        self.assertEqual(section.path, ['mydoc', 'mysection'])
+
+        # Ensure some of the necessary attributes are passed to the
+        # the section.
+        self.assertEqual(section.style.indentation,
+                         self.doc_structure.style.indentation)
+        self.assertEqual(section.translation_map,
+                         self.doc_structure.translation_map)
+        self.assertEqual(section.hrefs,
+                         self.doc_structure.hrefs)
+
+    def test_delete_section(self):
+        section = self.doc_structure.add_new_section('mysection')
+        self.assertEqual(
+            self.doc_structure.get_section('mysection'), section)
+        self.doc_structure.delete_section('mysection')
+        with self.assertRaises(KeyError):
+            section.get_section('mysection')
+
+    def test_create_sections_at_instantiation(self):
+        sections = ['intro', 'middle', 'end']
+        self.doc_structure = DocumentStructure(
+            self.name, section_names=sections)
+        # Ensure the sections are attached to the new document structure.
+        for section_name in sections:
+            section = self.doc_structure.get_section(section_name)
+            self.assertEqual(section.name, section_name)
+
+    def test_flush_structure(self):
+        section = self.doc_structure.add_new_section('mysection')
+        subsection = section.add_new_section('mysubsection')
+        self.doc_structure.writeln('1')
+        section.writeln('2')
+        subsection.writeln('3')
+        second_section = self.doc_structure.add_new_section('mysection2')
+        second_section.writeln('4')
+        contents = self.doc_structure.flush_structure()
+
+        # Ensure the contents were flushed out correctly
+        self.assertEqual(contents, six.b('1\n2\n3\n4\n'))
+
+    def test_flush_structure_hrefs(self):
+        section = self.doc_structure.add_new_section('mysection')
+        section.writeln('section contents')
+        self.doc_structure.hrefs['foo'] = 'www.foo.com'
+        section.hrefs['bar'] = 'www.bar.com'
+        contents = self.doc_structure.flush_structure()
+        self.assertIn(six.b('.. _foo: www.foo.com'), contents)
+        self.assertIn(six.b('.. _bar: www.bar.com'), contents)
+
+    def test_available_sections(self):
+        self.doc_structure.add_new_section('mysection')
+        self.doc_structure.add_new_section('mysection2')
+        self.assertEqual(
+            self.doc_structure.available_sections,
+            ['mysection', 'mysection2']
+        )
---
+++ tests/unit/docs/bcdoc/test_style.py
@@ -0,0 +1,231 @@
+# Copyright (c) 2012-2013 Mitch Garnaat http://garnaat.org/
+# Copyright 2012-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+#
+import unittest
+
+import six
+
+from bcdoc.style import ReSTStyle
+from bcdoc.restdoc import ReSTDocument
+
+
+class TestStyle(unittest.TestCase):
+
+    def test_spaces(self):
+        style = ReSTStyle(None, 4)
+        self.assertEqual(style.spaces(), '')
+        style.indent()
+        self.assertEqual(style.spaces(), '    ')
+        style.indent()
+        self.assertEqual(style.spaces(), '        ')
+        style.dedent()
+        self.assertEqual(style.spaces(), '    ')
+        style.dedent()
+        self.assertEqual(style.spaces(), '')
+        style.dedent()
+        self.assertEqual(style.spaces(), '')
+
+    def test_bold(self):
+        style = ReSTStyle(ReSTDocument())
+        style.bold('foobar')
+        self.assertEqual(style.doc.getvalue(), six.b('**foobar** '))
+
+    def test_italics(self):
+        style = ReSTStyle(ReSTDocument())
+        style.italics('foobar')
+        self.assertEqual(style.doc.getvalue(), six.b('*foobar* '))
+
+    def test_code(self):
+        style = ReSTStyle(ReSTDocument())
+        style.code('foobar')
+        self.assertEqual(style.doc.getvalue(), six.b('``foobar`` '))
+
+    def test_h1(self):
+        style = ReSTStyle(ReSTDocument())
+        style.h1('foobar fiebaz')
+        self.assertEqual(
+            style.doc.getvalue(),
+            six.b('\n\n*************\nfoobar fiebaz\n*************\n\n'))
+
+    def test_h2(self):
+        style = ReSTStyle(ReSTDocument())
+        style.h2('foobar fiebaz')
+        self.assertEqual(
+            style.doc.getvalue(),
+            six.b('\n\n=============\nfoobar fiebaz\n=============\n\n'))
+
+    def test_h3(self):
+        style = ReSTStyle(ReSTDocument())
+        style.h3('foobar fiebaz')
+        self.assertEqual(
+            style.doc.getvalue(),
+            six.b('\n\n-------------\nfoobar fiebaz\n-------------\n\n'))
+
+    def test_ref(self):
+        style = ReSTStyle(ReSTDocument())
+        style.ref('foobar', 'http://foo.bar.com')
+        self.assertEqual(style.doc.getvalue(),
+                         six.b(':doc:`foobar <http://foo.bar.com>`'))
+
+    def test_examples(self):
+        style = ReSTStyle(ReSTDocument())
+        self.assertTrue(style.doc.keep_data)
+        style.start_examples()
+        self.assertFalse(style.doc.keep_data)
+        style.end_examples()
+        self.assertTrue(style.doc.keep_data)
+
+    def test_codeblock(self):
+        style = ReSTStyle(ReSTDocument())
+        style.codeblock('foobar')
+        self.assertEqual(style.doc.getvalue(),
+                         six.b('::\n\n  foobar\n\n\n'))
+
+    def test_toctree_html(self):
+        style = ReSTStyle(ReSTDocument())
+        style.doc.target = 'html'
+        style.toctree()
+        style.tocitem('foo')
+        style.tocitem('bar')
+        self.assertEqual(
+            style.doc.getvalue(),
+            six.b('\n.. toctree::\n  :maxdepth: 1'
+                  '\n  :titlesonly:\n\n  foo\n  bar\n'))
+
+    def test_toctree_man(self):
+        style = ReSTStyle(ReSTDocument())
+        style.doc.target = 'man'
+        style.toctree()
+        style.tocitem('foo')
+        style.tocitem('bar')
+        self.assertEqual(style.doc.getvalue(),
+                         six.b('\n\n\n* foo\n\n\n* bar\n\n'))
+
+    def test_hidden_toctree_html(self):
+        style = ReSTStyle(ReSTDocument())
+        style.doc.target = 'html'
+        style.hidden_toctree()
+        style.hidden_tocitem('foo')
+        style.hidden_tocitem('bar')
+        self.assertEqual(
+            style.doc.getvalue(),
+            six.b('\n.. toctree::\n  :maxdepth: 1'
+                  '\n  :hidden:\n\n  foo\n  bar\n'))
+
+    def test_hidden_toctree_non_html(self):
+        style = ReSTStyle(ReSTDocument())
+        style.doc.target = 'man'
+        style.hidden_toctree()
+        style.hidden_tocitem('foo')
+        style.hidden_tocitem('bar')
+        self.assertEqual(
+            style.doc.getvalue(),
+            six.b(''))
+
+    def test_escape_href_link(self):
+        style = ReSTStyle(ReSTDocument())
+        style.start_a(attrs=[('href', 'http://example.org')])
+        style.doc.write('foo: the next bar')
+        style.end_a()
+        self.assertEqual(
+            style.doc.getvalue(),
+            six.b('`foo\\: the next bar`_ \n\n.. _foo\\: the next '
+                  'bar: http://example.org\n'))
+
+    def test_handle_no_text_hrefs(self):
+        style = ReSTStyle(ReSTDocument())
+        style.start_a(attrs=[('href', 'http://example.org')])
+        style.end_a()
+        self.assertEqual(style.doc.getvalue(),
+                         six.b('`<http://example.org>`_ '))
+
+    def test_sphinx_reference_label_html(self):
+        style = ReSTStyle(ReSTDocument())
+        style.doc.target = 'html'
+        style.sphinx_reference_label('foo', 'bar')
+        self.assertEqual(style.doc.getvalue(), six.b(':ref:`bar <foo>`'))
+
+    def test_sphinx_reference_label_html_no_text(self):
+        style = ReSTStyle(ReSTDocument())
+        style.doc.target = 'html'
+        style.sphinx_reference_label('foo')
+        self.assertEqual(style.doc.getvalue(), six.b(':ref:`foo <foo>`'))
+
+    def test_sphinx_reference_label_non_html(self):
+        style = ReSTStyle(ReSTDocument())
+        style.doc.target = 'man'
+        style.sphinx_reference_label('foo', 'bar')
+        self.assertEqual(style.doc.getvalue(), six.b('bar'))
+
+    def test_sphinx_reference_label_non_html_no_text(self):
+        style = ReSTStyle(ReSTDocument())
+        style.doc.target = 'man'
+        style.sphinx_reference_label('foo')
+        self.assertEqual(style.doc.getvalue(), six.b('foo'))
+
+    def test_table_of_contents(self):
+        style = ReSTStyle(ReSTDocument())
+        style.table_of_contents()
+        self.assertEqual(style.doc.getvalue(), six.b('.. contents:: '))
+
+    def test_table_of_contents_with_title(self):
+        style = ReSTStyle(ReSTDocument())
+        style.table_of_contents(title='Foo')
+        self.assertEqual(style.doc.getvalue(), six.b('.. contents:: Foo\n'))
+
+    def test_table_of_contents_with_title_and_depth(self):
+        style = ReSTStyle(ReSTDocument())
+        style.table_of_contents(title='Foo', depth=2)
+        self.assertEqual(style.doc.getvalue(),
+                         six.b('.. contents:: Foo\n   :depth: 2\n'))
+
+    def test_sphinx_py_class(self):
+        style = ReSTStyle(ReSTDocument())
+        style.start_sphinx_py_class('FooClass')
+        style.end_sphinx_py_class()
+        self.assertEqual(style.doc.getvalue(),
+                         six.b('\n\n.. py:class:: FooClass\n\n  \n\n'))
+
+    def test_sphinx_py_method(self):
+        style = ReSTStyle(ReSTDocument())
+        style.start_sphinx_py_method('method')
+        style.end_sphinx_py_method()
+        self.assertEqual(style.doc.getvalue(),
+                         six.b('\n\n.. py:method:: method\n\n  \n\n'))
+
+    def test_sphinx_py_method_with_params(self):
+        style = ReSTStyle(ReSTDocument())
+        style.start_sphinx_py_method('method', 'foo=None')
+        style.end_sphinx_py_method()
+        self.assertEqual(
+            style.doc.getvalue(),
+            six.b('\n\n.. py:method:: method(foo=None)\n\n  \n\n'))
+
+    def test_write_py_doc_string(self):
+        style = ReSTStyle(ReSTDocument())
+        docstring = (
+            'This describes a function\n'
+            ':param foo: Describes foo\n'
+            'returns: None'
+        )
+        style.write_py_doc_string(docstring)
+        self.assertEqual(style.doc.getvalue(), six.b(docstring + '\n'))
