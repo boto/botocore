6a20b7f7e898c5ed025d7ea06cf3a2777894e06a
kyleknap <kyleknap@amazon.com>
2015-10-01 18:21:42 -0700

    Fix circular reference in docs
    Removed dependency of session from docs package

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,10 @@
 # serve to show the default.
 
 import sys, os
+from botocore.session import get_session
 from botocore.docs import generate_docs
 
-generate_docs(os.path.dirname(os.path.abspath(__file__)))
+generate_docs(os.path.dirname(os.path.abspath(__file__)), get_session())
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
