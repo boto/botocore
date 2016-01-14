d1adbdaf8bb08043d7f4119db1e51f803790463b
James Saryerwinnie <js@jamesls.com>
2016-01-13 16:13:05 -0800

    Add handler to modify CopySource documentation
    This ensures the alternate dict form is also documented
    for CopySource.

--- botocore/handlers.py
+++ botocore/handlers.py
@@ -268,6 +268,35 @@ def add_expect_header(model, params, **kwargs):
             params['headers']['Expect'] = '100-continue'
 
 
+def document_copy_source_form(section, event_name, **kwargs):
+    if 'request-example' in event_name:
+        parent = section.get_section('structure-value')
+        param_line = parent.get_section('CopySource')
+        value_portion = param_line.get_section('member-value')
+        value_portion.clear_text()
+        value_portion.write("'string' or {'Bucket': 'string', "
+                            "'Key': 'string', 'VersionId': 'string'}")
+    elif 'request-params' in event_name:
+        param_section = section.get_section('CopySource')
+        type_section = param_section.get_section('param-type')
+        type_section.clear_text()
+        type_section.write(':type CopySource: str or dict')
+        doc_section = param_section.get_section('param-documentation')
+        doc_section.clear_text()
+        doc_section.write(
+            "The name of the source bucket, key name of the source object, "
+            "and optional version ID of the source object.  You can either "
+            "provide this value as a string or a dictionary.  The "
+            "string form is {bucket}/{key} or "
+            "{bucket}/{key}?versionId={versionId} if you want to copy a "
+            "specific version.  You can also provide this value as a "
+            "dictionary.  The dictionary format is recommended over "
+            "the string format because it is more explicit.  The dictionary "
+            "format is: {'Bucket': 'bucket', 'Key': 'key', 'VersionId': 'id'}."
+            "  Note that the VersionId key is optional and may be omitted."
+        )
+
+
 def support_dict_copy_source(params, context, **kwargs):
     source = params.get('CopySource')
     if source is None or not isinstance(source, dict):
@@ -602,8 +631,7 @@ BUILTIN_HANDLERS = [
      support_dict_copy_source),
     ('before-parameter-build.s3.UploadPartCopy',
      support_dict_copy_source),
-
-
+    ('docs.*.s3.CopyObject.complete-section', document_copy_source_form),
 
     ('before-call.s3', add_expect_header),
     ('before-call.glacier', add_glacier_version),
--- tests/functional/docs/__init__.py
+++ tests/functional/docs/__init__.py
@@ -69,11 +69,17 @@ class BaseDocsFunctionalTest(unittest.TestCase):
         return self.get_parameter_document_block(
             param_name, method_contents)
 
+    def get_docstring_for_method(self, service_name, method_name):
+        contents = ServiceDocumenter(
+            service_name, self._session).document_service()
+        method_contents = self.get_method_document_block(
+            method_name, contents)
+        return method_contents
+
     def assert_is_documented_as_autopopulated_param(
             self, service_name, method_name, param_name, doc_string=None):
         contents = ServiceDocumenter(
             service_name, self._session).document_service()
-        # Pick an arbitrary method that uses AccountId.
         method_contents = self.get_method_document_block(
             method_name, contents)
 
