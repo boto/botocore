c247d86bd4cd799f90bb39f5f3893fb18de2eb3e
Ray Luo <rayluo@amazon.com>
2016-02-11 15:00:06 -0800

    Use IS for object identity

--- botocore/session.py
+++ botocore/session.py
@@ -787,7 +787,7 @@ class Session(object):
         event_emitter = self.get_component('event_emitter')
         response_parser_factory = self.get_component(
             'response_parser_factory')
-        if config and config.signature_version == UNSIGNED:
+        if config and config.signature_version is UNSIGNED:
             credentials = None
         elif aws_secret_access_key is not None:
             credentials = botocore.credentials.Credentials(
