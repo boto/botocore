38e86498a7789c536cb065febac231f4a437c2de
James Saryerwinnie <js@jamesls.com>
2016-05-12 13:50:05 -0700

    Cleanup pep8 issues

--- tests/integration/test_waiters.py
+++ tests/integration/test_waiters.py
@@ -10,13 +10,12 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-import time
 from tests import unittest, random_chars
 
 from nose.plugins.attrib import attr
 
 import botocore.session
-from botocore.exceptions import ClientError, WaiterError
+from botocore.exceptions import WaiterError
 
 
 # This is the same test as above, except using the client interface.
