b0c801bd19dc18a82433721c795eed7f5e5d6b74
Fabio Alessandro Locati <fabio@locati.cc>
2016-05-28 09:58:16 +0100

    Remove docutil dependency from setup.py
    Docutil is used only in the following places:
    
    * botocore/docs/bcdoc/textwriter.py
    
    And is also referenced in:
    * setup.py
    * setup.cfg
    * requirements.txt
    
    Since during the setup.py execution the documentation is not generated,
    I think we should not require docutil to be installed. We could create a
    secondary target to create the documentation, but this is a different
    problem/opportunity/situation.

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ from setuptools import setup, find_packages
 
 
 requires = ['jmespath>=0.7.1,<1.0.0',
-            'python-dateutil>=2.1,<3.0.0',
-            'docutils>=0.10']
+            'python-dateutil>=2.1,<3.0.0']
 
 
 if sys.version_info[:2] == (2, 6):
