6a51bd253e2369de14dd5b4368296086abccca34
Andrey Kislyuk <kislyuk@gmail.com>
2016-08-13 23:24:40 -0700

    Make sure __file__ behaves reasonably

--- botocore/errorfactory.py
+++ botocore/errorfactory.py
@@ -3,10 +3,13 @@ import sys
 
 class ClientErrorFactory:
     cache = {}
+    _file = __file__
 
     def __getattr__(self, attr):
         if attr == '__all__':
             return list(self.cache)
+        if attr == '__file__':
+            return self._file
         if attr == '__path__' or attr == '__loader__':
             return None
         if attr not in self.cache:
