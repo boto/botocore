47dba5b3aac786405212275463a476037ddef564
James Saryerwinnie <js@jamesls.com>
2016-08-31 10:43:44 -0700

    Add retry info to ClientError text
    Error messages from ClientErrors will now look like:
    
    An error occurred (C) when calling the O operation (max_retry=4 reached): M
    
    If the error response does not have retry information the client error
    is unchanged.

--- botocore/exceptions.py
+++ botocore/exceptions.py
@@ -342,16 +342,29 @@ class UnsupportedSignatureVersionError(BotoCoreError):
 class ClientError(Exception):
     MSG_TEMPLATE = (
         'An error occurred ({error_code}) when calling the {operation_name} '
-        'operation: {error_message}')
+        'operation{retry_info}: {error_message}')
 
     def __init__(self, error_response, operation_name):
+        retry_info = self._get_retry_info(error_response)
         msg = self.MSG_TEMPLATE.format(
             error_code=error_response['Error'].get('Code', 'Unknown'),
             error_message=error_response['Error'].get('Message', 'Unknown'),
-            operation_name=operation_name)
+            operation_name=operation_name,
+            retry_info=retry_info,
+        )
         super(ClientError, self).__init__(msg)
         self.response = error_response
         self.operation_name = operation_name
+
+    def _get_retry_info(self, response):
+        retry_info = ''
+        if 'ResponseMetadata' in response:
+            metadata = response['ResponseMetadata']
+            if metadata.get('MaxAttemptsReached', False):
+                if 'RetryAttempts' in metadata:
+                    retry_info = (' (max_retries=%s reached)' %
+                                  metadata['RetryAttempts'])
+        return retry_info
 
 
 class UnsupportedTLSVersionWarning(Warning):
