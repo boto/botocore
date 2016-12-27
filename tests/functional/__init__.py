4af767a04faad2575b40901d6051a98f2b45833a
James Saryerwinnie <js@jamesls.com>
2016-12-27 13:20:07 -0800

    Ensure no creds are automatically picked up in functional tests
    There's a failing RDS functional test.  It looks like this failed
    because travis CI does not have any credentials configured (which is
    expected).  However, because credentials are configured on the
    dev laptops, you won't see this failure locally.
    
    To fix this issue, this commit ensures that all the default credentials
    won't be picked up.  This help brings the travis environment closer
    to a dev environment.
    
    This this commit I now see the failing RDS test locally, even if I
    have ~/.aws/config or ~/.aws/credentials configured.

--- tests/functional/__init__.py
+++ tests/functional/__init__.py
@@ -10,3 +10,31 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+import os
+import uuid
+
+_ORIGINAL = os.environ.copy()
+# These are environment variables that allow users to control
+# the location of config files used by botocore.
+_CONFIG_FILE_ENV_VARS = [
+    'AWS_CONFIG_FILE',
+    'AWS_SHARED_CREDENTIALS_FILE',
+    'BOTO_CONFIG',
+]
+_CREDENTIAL_ENV_VARS = [
+    'AWS_ACCESS_KEY_ID',
+    'AWS_SECRET_ACCESS_KEY',
+    'AWS_SESSION_TOKEN',
+]
+
+
+def setup_package():
+    random_file = str(uuid.uuid4())
+    for varname in _CONFIG_FILE_ENV_VARS:
+        os.environ[varname] = random_file
+    for credvar in _CREDENTIAL_ENV_VARS:
+        os.environ.pop(credvar, None)
+
+
+def teardown_package():
+    os.environ = _ORIGINAL
