f9686d0d00e8f900c8d6900e23fbffe3da1681c9
JordonPhillips <phjordon@amazon.com>
2017-05-30 08:29:30 -0700

    Test restricted regions don't use virtual host
    This adds tests to verify that for certain restricted regions we
    don't use virtual host style addressing by default with sigv2. There
    had been tests that covered that case before, but the switch to sigv4
    by default eliminated that branch coverage.

--- tests/functional/test_s3.py
+++ tests/functional/test_s3.py
@@ -420,6 +420,17 @@ def test_correct_url_used_for_s3():
         region='us-gov-west-1', bucket='bucket', key='key',
         s3_config=virtual_hosting,
         expected_url='https://bucket.s3-us-gov-west-1.amazonaws.com/key')
+    
+    # Test restricted regions not do virtual host by default
+    yield t.case(
+        region='us-gov-west-1', bucket='bucket', key='key',
+        signature_version='s3',
+        expected_url='https://s3-us-gov-west-1.amazonaws.com/bucket/key')
+    yield t.case(
+        region='fips-us-gov-west-1', bucket='bucket', key='key',
+        signature_version='s3',
+        expected_url='https://s3-fips-us-gov-west-1.amazonaws.com/bucket/key')
+
 
     # Test path style addressing.
     path_style = {'addressing_style': 'path'}
