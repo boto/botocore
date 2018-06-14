0704ceb40e21c1bed7116671f7f8acede8216c01
Jordan Guymon <jogu.sd@gmail.com>
2018-06-14 15:15:38 -0700

    Add http session tests

--- botocore/http_session.py
+++ botocore/http_session.py
@@ -19,14 +19,62 @@ except ImportError:
 
 filter_ssl_warnings()
 logger = logging.getLogger(__name__)
+DEFAULT_TIMEOUT = 60
+MAX_POOL_CONNECTIONS = 10
+DEFAULT_CA_BUNDLE = 'TODO'
+
+try:
+    from certifi import where
+except ImportError:
+    def where():
+        return DEFAULT_CA_BUNDLE
+
+
+def construct_basic_auth(username, password):
+    auth_str = '{0}:{1}'.format(username, password)
+    encoded_str = b64encode(auth_str.encode('ascii')).strip().decode()
+    return 'Basic {0}'.format(encoded_str)
+
+
+def get_auth_from_url(url):
+    parsed_url = urlparse(url)
+    try:
+        return unquote(parsed_url.username), unquote(parsed_url.password)
+    except (AttributeError, TypeError):
+        return None, None
+
+
+def get_proxy_headers(proxy_url):
+    headers = {}
+    username, password = get_auth_from_url(proxy_url)
+    if username and password:
+        basic_auth = construct_basic_auth(username, password)
+        headers['Proxy-Authorization'] = basic_auth
+    return headers
+
+
+def fix_proxy_url(proxy_url):
+    if proxy_url.startswith('http:') or proxy_url.startswith('https:'):
+        return proxy_url
+    elif proxy_url.startswith('//'):
+        return 'http:' + proxy_url
+    else:
+        return 'http://' + proxy_url
+
+
+def get_cert_path(verify):
+    if verify is not True:
+        return verify
+
+    return where()
 
 
 class Urllib3Session(object):
     def __init__(self,
                  verify=True,
                  proxies=None,
-                 timeout=None,
-                 max_pool_connections=None,
+                 timeout=DEFAULT_TIMEOUT,
+                 max_pool_connections=MAX_POOL_CONNECTIONS,
     ):
         self._verify = verify
         self._proxies = proxies or {}
@@ -35,30 +83,10 @@ class Urllib3Session(object):
         self._proxy_managers = {}
         self._http_pool = PoolManager(maxsize=self._max_pool_connections)
 
-    def _construct_basic_auth(self, username, password):
-        auth_str = '{0}:{1}'.format(username, password)
-        encoded_str = b64encode(auth_str.encode('ascii')).strip().decode()
-        return 'Basic {0}'.format(encoded_str)
-
-    def _get_auth_from_url(self, url):
-        parsed_url = urlparse(url)
-        try:
-            return unquote(parsed_url.username), unquote(parsed_url.password)
-        except (AttributeError, TypeError):
-            return '', ''
-
-    def _get_proxy_headers(self, proxy_url):
-        headers = {}
-        username, password = self._get_auth_from_url(proxy_url)
-        if username and password:
-            basic_auth = self._construct_basic_auth(username, password)
-            headers['Proxy-Authorization'] = basic_auth
-        return headers
-
     def _get_proxy_manager(self, proxy_url):
-        proxy_url = self._fix_proxy(proxy_url)
+        proxy_url = fix_proxy_url(proxy_url)
         if proxy_url not in self._proxy_managers:
-            proxy_headers = self._get_proxy_headers(proxy_url)
+            proxy_headers = get_proxy_headers(proxy_url)
             self._proxy_managers[proxy_url] = proxy_from_url(
                 proxy_url,
                 proxy_headers=proxy_headers,
@@ -66,14 +94,6 @@ class Urllib3Session(object):
             )
 
         return self._proxy_managers[proxy_url]
-
-    def _fix_proxy(self, proxy_url):
-        if proxy_url.startswith('http:') or proxy_url.startswith('https:'):
-            return proxy_url
-        elif proxy_url.startswith('//'):
-            return 'http:' + proxy_url
-        else:
-            return 'http://' + proxy_url
 
     def _get_connection(self, url):
         scheme = urlparse(url.lower()).scheme
@@ -86,42 +106,18 @@ class Urllib3Session(object):
 
         return connection_manager.connection_from_url(url)
 
-    def _verify_cert(self, conn, url, verify, cert):
+    def _verify_cert(self, conn, url, verify):
         if url.lower().startswith('https') and verify:
-
-            cert_loc = None
-
-            # Allow self-specified cert location.
-            if verify is not True:
-                cert_loc = verify
-
-            if not cert_loc:
-                # TODO default cert path
-                import certifi
-                cert_loc = certifi.where()
-
-            if not cert_loc:
-                raise Exception("Could not find a suitable SSL CA certificate bundle.")
-
             conn.cert_reqs = 'CERT_REQUIRED'
-            conn.ca_certs = cert_loc
+            conn.ca_certs = get_cert_path(verify)
         else:
             conn.cert_reqs = 'CERT_NONE'
             conn.ca_certs = None
 
-        if cert:
-            if six.PY3:
-                basestring = (str, bytes)
-            if not isinstance(cert, basestring):
-                conn.cert_file = cert[0]
-                conn.key_file = cert[1]
-            else:
-                conn.cert_file = cert
-
     def send(self, request, streaming=False):
         try:
             conn = self._get_connection(request.url)
-            self._verify_cert(conn, request.url, self._verify, None)
+            self._verify_cert(conn, request.url, self._verify)
             urllib_response = conn.urlopen(
                 method=request.method,
                 url=request.url,
@@ -142,12 +138,6 @@ class Urllib3Session(object):
             if not streaming:
                 # Cause the raw stream to be exhausted immediatly
                 http_response.content
-
-            # We techniclly don't use any of these
-            # http_response.connection = conn
-            # http_response.encoding = None  # TODO get from headers?
-            # http_response.request = request
-            # http_response.reason = urllib_response.reason
 
             return http_response
         except (NewConnectionError, socket.gaierror) as e:
---
+++ tests/unit/test_http_session.py
@@ -0,0 +1,169 @@
+from mock import patch, Mock
+from tests import unittest
+from nose.tools import raises
+from urllib3.exceptions import NewConnectionError
+
+from botocore.http_session import (
+    construct_basic_auth,
+    get_auth_from_url,
+    get_proxy_headers,
+    get_cert_path,
+    fix_proxy_url,
+)
+
+from botocore.vendored import six
+from botocore.awsrequest import AWSRequest
+from botocore.http_session import Urllib3Session
+from botocore.exceptions import ConnectionClosedError, EndpointConnectionError
+
+
+class TestHttpSessionUtils(unittest.TestCase):
+
+    def setUp(self):
+        self.url = 'http://localhost/'
+        self.auth_url = 'http://user:pass@localhost/'
+
+    def test_construct_basic_auth(self):
+        auth = construct_basic_auth('user', 'pass')
+        self.assertEqual('Basic dXNlcjpwYXNz', auth)
+
+    def test_get_auth_from_url(self):
+        username, password = get_auth_from_url(self.auth_url)
+        self.assertEqual('user', username)
+        self.assertEqual('pass', password)
+
+    def test_get_auth_from_url_no_auth(self):
+        username, password = get_auth_from_url(self.url)
+        self.assertIsNone(username)
+        self.assertIsNone(password)
+
+    def test_get_proxy_headers(self):
+        headers = get_proxy_headers(self.auth_url)
+        proxy_auth = headers.get('Proxy-Authorization')
+        self.assertEqual('Basic dXNlcjpwYXNz', proxy_auth)
+
+    def test_fix_proxy_url_no_slashes(self):
+        url = 'localhost:8081/'
+        fixed_url = fix_proxy_url(url)
+        self.assertEqual('http://localhost:8081/', fixed_url)
+
+    def test_fix_proxy_url_no_protocol(self):
+        url = '//localhost:8081/'
+        fixed_url = fix_proxy_url(url)
+        self.assertEqual('http://localhost:8081/', fixed_url)
+
+    def test_fix_proxy_url_has_protocol_http(self):
+        url = 'http://localhost:8081/'
+        fixed_url = fix_proxy_url(url)
+        self.assertEqual(url, fixed_url)
+
+    def test_fix_proxy_url_has_protocol_https(self):
+        url = 'https://localhost:8081/'
+        fixed_url = fix_proxy_url(url)
+        self.assertEqual(url, fixed_url)
+
+    def test_get_cert_path_path(self):
+        path = '/some/path'
+        cert_path = get_cert_path(path)
+        self.assertEqual(path, cert_path)
+
+    def test_get_cert_path_certifi_or_default(self):
+        with patch('botocore.http_session.where') as where:
+            path = '/bundle/path'
+            where.return_value = path
+            cert_path = get_cert_path(True)
+            self.assertEqual(path, cert_path)
+
+
+class TestUrllib3Session(unittest.TestCase):
+
+    def setUp(self):
+        self.request = AWSRequest(
+            method='GET',
+            url='http://example.com',
+            headers={},
+            data=b'',
+        )
+
+        self.response = Mock()
+        self.response.headers = {}
+        self.response.stream.return_value = b''
+
+        self.pool_manager = Mock()
+        self.connection = Mock()
+        self.connection.urlopen.return_value = self.response
+        self.pool_manager.connection_from_url.return_value = self.connection
+
+        self.pool_patch = patch('botocore.http_session.PoolManager')
+        self.proxy_patch = patch('botocore.http_session.proxy_from_url')
+        self.pool_manager_cls = self.pool_patch.start()
+        self.proxy_manager_fun = self.proxy_patch.start()
+        self.pool_manager_cls.return_value = self.pool_manager
+        self.proxy_manager_fun.return_value = self.pool_manager
+
+    def tearDown(self):
+        self.pool_patch.stop()
+
+    def asert_request_sent(self, headers=None, body=None):
+        if headers is None:
+            headers = {}
+
+        self.connection.urlopen.assert_called_once_with(
+            method=self.request.method,
+            url=self.request.url,
+            body=body,
+            headers=headers,
+            retries=False,
+            assert_same_host=False,
+            preload_content=False,
+            decode_content=False,
+        )
+
+    def test_forwards_max_pool_size(self):
+        Urllib3Session(max_pool_connections=22)
+        self.pool_manager_cls.assert_called_with(maxsize=22)
+
+    def test_basic_request(self):
+        session = Urllib3Session()
+        session.send(self.request.prepare())
+        self.asert_request_sent()
+        self.response.stream.assert_called_once_with()
+
+    def test_basic_streaming_request(self):
+        session = Urllib3Session()
+        session.send(self.request.prepare(), streaming=True)
+        self.asert_request_sent()
+        self.response.stream.assert_not_called()
+
+    def test_basic_https_request(self):
+        session = Urllib3Session()
+        self.request.url = 'https://example.com'
+        session.send(self.request.prepare())
+        self.asert_request_sent()
+
+    def test_basic_proxy_request(self):
+        proxies = {'https': 'http://proxy.com'}
+        session = Urllib3Session(proxies=proxies)
+        self.request.url = 'https://example.com'
+        session.send(self.request.prepare())
+        self.proxy_manager_fun.assert_any_call(
+            proxies['https'],
+            proxy_headers={},
+            maxsize=10,
+        )
+        self.asert_request_sent()
+
+    def make_request_with_error(self, error):
+        self.connection.urlopen.side_effect = error
+        session = Urllib3Session()
+        session.send(self.request.prepare())
+
+    @raises(EndpointConnectionError)
+    def test_catches_new_connection_error(self):
+        error = NewConnectionError(None, None)
+        self.make_request_with_error(error)
+
+    @raises(ConnectionClosedError)
+    def test_catches_bad_status_line(self):
+        error = six.moves.http_client.BadStatusLine(None)
+        self.make_request_with_error(error)
