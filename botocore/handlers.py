boto/botocore

commit ba514720588c520fdf5ed0c77ddc76e4455246d0

Register with service id

botocore/handlers.py
@@ -37,6 +37,7 @@ from botocore.exceptions import AliasConflictParameterError
 from botocore.exceptions import UnsupportedTLSVersionWarning
 from botocore.utils import percent_encode, SAFE_CHARS
 from botocore.utils import switch_host_with_param
+from botocore.utils import hyphenize_service_id
 
 from botocore import retryhandler
 from botocore import utils
@@ -273,6 +274,8 @@ def _needs_s3_sse_customization(params, sse_member_prefix):
 def register_retries_for_service(service_data, session,
                                  service_name, **kwargs):
     loader = session.get_component('data_loader')
+    service_id = service_data.get('metadata', {}).get('serviceId')
+    service_event_name = hyphenize_service_id(service_id)
     endpoint_prefix = service_data.get('metadata', {}).get('endpointPrefix')
     if endpoint_prefix is None:
         logger.debug("Not registering retry handlers, could not endpoint "
@@ -284,11 +287,10 @@ def register_retries_for_service(service_data, session,
     logger.debug("Registering retry handlers for service: %s", service_name)
     handler = retryhandler.create_retry_handler(
         config, endpoint_prefix)
-    unique_id = 'retry-config-%s' % endpoint_prefix
-    session.register('needs-retry.%s' % endpoint_prefix,
+    unique_id = 'retry-config-%s' % service_event_name
+    session.register('needs-retry.%s' % service_event_name,
                      handler, unique_id=unique_id)
-    _register_for_operations(config, session,
-                             service_name=endpoint_prefix)
+    _register_for_operations(config, session, service_event_name)
 
 
 def _load_retry_config(loader, endpoint_prefix):
@@ -299,7 +301,7 @@ def _load_retry_config(loader, endpoint_prefix):
     return retry_config
 
 
-def _register_for_operations(config, session, service_name):
+def _register_for_operations(config, session, service_event_name):
     # There's certainly a tradeoff for registering the retry config
     # for the operations when the service is created.  In practice,
     # there aren't a whole lot of per operation retry configs so
@@ -308,8 +310,8 @@ def _register_for_operations(config, session, service_name):
         if key == '__default__':
             continue
         handler = retryhandler.create_retry_handler(config, key)
-        unique_id = 'retry-config-%s-%s' % (service_name, key)
-        session.register('needs-retry.%s.%s' % (service_name, key),
+        unique_id = 'retry-config-%s-%s' % (service_event_name, key)
+        session.register('needs-retry.%s.%s' % (service_event_name, key),
                          handler, unique_id=unique_id)
 
 

botocore/model.py
@@ -13,7 +13,7 @@
 """Abstractions to interact with service models."""
 from collections import defaultdict
 
-from botocore.utils import CachedProperty, instance_cache
+from botocore.utils import CachedProperty, instance_cache, hyphenize_service_id
 from botocore.compat import OrderedDict
 
 
@@ -38,6 +38,11 @@ class InvalidShapeReferenceError(Exception):
 
 class UndefinedModelAttributeError(Exception):
     pass
+
+
+class ServiceId(str):
+    def hyphenize(self):
+        return hyphenize_service_id(self)
 
 
 class Shape(object):
@@ -286,7 +291,7 @@ class ServiceModel(object):
 
     @CachedProperty
     def service_id(self):
-        return self._get_metadata_property('serviceId')
+        return ServiceId(self._get_metadata_property('serviceId'))
 
     @CachedProperty
     def signing_name(self):

tests/unit/test_handlers.py
@@ -31,7 +31,7 @@ from botocore.docs.bcdoc.restdoc import DocumentStructure
 from botocore.docs.params import RequestParamsDocumenter
 from botocore.docs.example import RequestExampleDocumenter
 from botocore.hooks import HierarchicalEmitter
-from botocore.model import OperationModel, ServiceModel
+from botocore.model import OperationModel, ServiceModel, ServiceId
 from botocore.model import DenormalizedStructureBuilder
 from botocore.signers import RequestSigner
 from botocore.credentials import Credentials
@@ -139,7 +139,8 @@ class TestHandlers(BaseSessionTest):
         credentials = Credentials('key', 'secret')
         event_emitter = HierarchicalEmitter()
         request_signer = RequestSigner(
-            'ec2', 'us-east-1', 'ec2', 'v4', credentials, event_emitter)
+            ServiceId('ec2'), 'us-east-1', 'ec2', 'v4',
+            credentials, event_emitter)
         handlers.inject_presigned_url_ec2(
             params, request_signer, operation_model)
         self.assertEqual(params['body']['PresignedUrl'], 'https://foo')
@@ -156,7 +157,8 @@ class TestHandlers(BaseSessionTest):
         credentials = Credentials('key', 'secret')
         event_emitter = HierarchicalEmitter()
         request_signer = RequestSigner(
-            'ec2', 'us-east-1', 'ec2', 'v4', credentials, event_emitter)
+            ServiceId('ec2'), 'us-east-1', 'ec2', 'v4', credentials,
+            event_emitter)
         handlers.inject_presigned_url_ec2(
             params, request_signer, operation_model)
         self.assertEqual(params['body']['PresignedUrl'], 'https://foo')
@@ -169,7 +171,8 @@ class TestHandlers(BaseSessionTest):
         credentials = Credentials('key', 'secret')
         event_emitter = HierarchicalEmitter()
         request_signer = RequestSigner(
-            'rds', 'us-east-1', 'rds', 'v4', credentials, event_emitter)
+            ServiceId('rds'), 'us-east-1', 'rds', 'v4', credentials,
+            event_emitter)
         handlers.inject_presigned_url_rds(
             params, request_signer, operation_model)
         self.assertEqual(params['body']['PreSignedUrl'], 'https://foo')
@@ -186,7 +189,8 @@ class TestHandlers(BaseSessionTest):
         credentials = Credentials('key', 'secret')
         event_emitter = HierarchicalEmitter()
         request_signer = RequestSigner(
-            'rds', 'us-east-1', 'rds', 'v4', credentials, event_emitter)
+            ServiceId('rds'), 'us-east-1', 'rds', 'v4', credentials,
+            event_emitter)
         handlers.inject_presigned_url_rds(
             params, request_signer, operation_model)
         self.assertEqual(params['body']['PreSignedUrl'], 'https://foo')
@@ -198,7 +202,8 @@ class TestHandlers(BaseSessionTest):
         credentials = Credentials('key', 'secret')
         event_emitter = HierarchicalEmitter()
         request_signer = RequestSigner(
-            'ec2', 'us-east-1', 'ec2', 'v4', credentials, event_emitter)
+            ServiceId('ec2'), 'us-east-1', 'ec2', 'v4', credentials,
+            event_emitter)
         request_dict = {}
         params = {'SourceRegion': 'us-west-2'}
         request_dict['body'] = params
@@ -250,7 +255,8 @@ class TestHandlers(BaseSessionTest):
         credentials = Credentials('key', 'secret')
         event_emitter = HierarchicalEmitter()
         request_signer = RequestSigner(
-            'ec2', actual_region, 'ec2', 'v4', credentials, event_emitter)
+            ServiceId('ec2'), actual_region, 'ec2', 'v4', credentials,
+            event_emitter)
         request_dict = {}
         params = {
             'SourceRegion': 'us-west-2',
@@ -279,7 +285,8 @@ class TestHandlers(BaseSessionTest):
         credentials = Credentials('key', 'secret')
         event_emitter = HierarchicalEmitter()
         request_signer = RequestSigner(
-            'rds', 'us-east-1', 'rds', 'v4', credentials, event_emitter)
+            ServiceId('rds'), 'us-east-1', 'rds', 'v4', credentials,
+            event_emitter)
         request_dict = {}
         params = {'SourceRegion': 'us-west-2'}
         request_dict['body'] = params
@@ -305,7 +312,9 @@ class TestHandlers(BaseSessionTest):
         credentials = Credentials('key', 'secret')
         event_emitter = HierarchicalEmitter()
         request_signer = RequestSigner(
-            'rds', 'us-east-1', 'rds', 'v4', credentials, event_emitter)
+            ServiceId('rds'), 'us-east-1', 'rds', 'v4', credentials,
+            event_emitter
+        )
         request_dict = {}
         params = {'SourceRegion': 'us-west-2'}
         request_dict['body'] = params
@@ -328,7 +337,8 @@ class TestHandlers(BaseSessionTest):
         credentials = Credentials('key', 'secret')
         event_emitter = HierarchicalEmitter()
         request_signer = RequestSigner(
-            'rds', 'us-east-1', 'rds', 'v4', credentials, event_emitter)
+            ServiceId('rds'), 'us-east-1', 'rds', 'v4', credentials,
+            event_emitter)
         request_dict = {}
         params = {'SourceRegion': 'us-west-2', 'PreSignedUrl': 'https://foo'}
         request_dict['body'] = params
@@ -351,7 +361,8 @@ class TestHandlers(BaseSessionTest):
         credentials = Credentials('key', 'secret')
         event_emitter = HierarchicalEmitter()
         request_signer = RequestSigner(
-            'rds', 'us-east-1', 'rds', 'v4', credentials, event_emitter)
+            ServiceId('rds'), 'us-east-1', 'rds', 'v4', credentials,
+            event_emitter)
         request_dict = {}
         params = {'SourceRegion': 'us-west-2'}
         request_dict['body'] = params
@@ -375,7 +386,8 @@ class TestHandlers(BaseSessionTest):
         credentials = Credentials('key', 'secret')
         event_emitter = HierarchicalEmitter()
         request_signer = RequestSigner(
-            'rds', 'us-east-1', 'rds', 'v4', credentials, event_emitter)
+            ServiceId('rds'), 'us-east-1', 'rds', 'v4', credentials,
+            event_emitter)
         handlers.inject_presigned_url_rds(
             params=params,
             request_signer=request_signer,
@@ -389,7 +401,8 @@ class TestHandlers(BaseSessionTest):
         credentials = Credentials('key', 'secret')
         event_emitter = HierarchicalEmitter()
         request_signer = RequestSigner(
-            'rds', 'us-east-1', 'rds', 'v4', credentials, event_emitter)
+            ServiceId('rds'), 'us-east-1', 'rds', 'v4', credentials,
+            event_emitter)
         request_dict = {}
         params = {'SourceRegion': 'us-west-2'}
         request_dict['body'] = params
@@ -537,7 +550,7 @@ class TestHandlers(BaseSessionTest):
         self.assertEqual(params, result)
 
     def test_register_retry_for_handlers_with_no_endpoint_prefix(self):
-        no_endpoint_prefix = {'metadata': {}}
+        no_endpoint_prefix = {'metadata': {'serviceId': 'foo'}}
         session = mock.Mock()
         handlers.register_retries_for_service(service_data=no_endpoint_prefix,
                                               session=mock.Mock(),
@@ -546,7 +559,10 @@ class TestHandlers(BaseSessionTest):
 
     def test_register_retry_handlers(self):
         service_data = {
-            'metadata': {'endpointPrefix': 'foo'},
+            'metadata': {
+                'endpointPrefix': 'foo',
+                'serviceId': 'foo',
+            },
         }
         session = mock.Mock()
         loader = mock.Mock()
@@ -1059,7 +1075,7 @@ class TestAddMD5(BaseMD5Test):
     def test_adds_md5_when_v4(self):
         credentials = Credentials('key', 'secret')
         request_signer = RequestSigner(
-            's3', 'us-east-1', 's3', 'v4', credentials, mock.Mock())
+            ServiceId('s3'), 'us-east-1', 's3', 'v4', credentials, mock.Mock())
         request_dict = {'body': b'bar',
                         'url': 'https://s3.us-east-1.amazonaws.com',
                         'method': 'PUT',
@@ -1072,7 +1088,8 @@ class TestAddMD5(BaseMD5Test):
     def test_adds_md5_when_s3v4(self):
         credentials = Credentials('key', 'secret')
         request_signer = RequestSigner(
-            's3', 'us-east-1', 's3', 's3v4', credentials, mock.Mock())
+            ServiceId('s3'), 'us-east-1', 's3', 's3v4', credentials,
+            mock.Mock())
         request_dict = {'body': b'bar',
                         'url': 'https://s3.us-east-1.amazonaws.com',
                         'method': 'PUT',
@@ -1101,7 +1118,7 @@ class TestAddMD5(BaseMD5Test):
     def test_add_md5_raises_error_when_md5_unavailable(self):
         credentials = Credentials('key', 'secret')
         request_signer = RequestSigner(
-            's3', 'us-east-1', 's3', 's3', credentials, mock.Mock())
+            ServiceId('s3'), 'us-east-1', 's3', 's3', credentials, mock.Mock())
         request_dict = {'body': b'bar',
                         'url': 'https://s3.us-east-1.amazonaws.com',
                         'method': 'PUT',
@@ -1115,7 +1132,7 @@ class TestAddMD5(BaseMD5Test):
     def test_adds_md5_when_s3v2(self):
         credentials = Credentials('key', 'secret')
         request_signer = RequestSigner(
-            's3', 'us-east-1', 's3', 's3', credentials, mock.Mock())
+            ServiceId('s3'), 'us-east-1', 's3', 's3', credentials, mock.Mock())
         request_dict = {'body': b'bar',
                         'url': 'https://s3.us-east-1.amazonaws.com',
                         'method': 'PUT',

tests/unit/test_model.py
@@ -30,6 +30,16 @@ def test_missing_model_attribute_raises_exception():
         yield _test_attribute_raise_exception, name
 
 
+class TestServiceId(unittest.TestCase):
+    def test_hypenize_replaces_spaces(self):
+        self.assertEqual(
+            model.ServiceId('my service').hyphenize(), 'my-service'
+        )
+
+    def test_hyphenize_lower_cases(self):
+        self.assertEqual(model.ServiceId('MyService').hyphenize(), 'myservice')
+
+
 class TestServiceModel(unittest.TestCase):
 
     def setUp(self):
@@ -60,6 +70,10 @@ class TestServiceModel(unittest.TestCase):
 
     def test_service_id(self):
         self.assertEqual(self.service_model.service_id, 'MyService')
+
+    def test_hyphenize_service_id(self):
+        self.assertEqual(
+            self.service_model.service_id.hyphenize(), 'myservice')
 
     def test_operation_does_not_exist(self):
         with self.assertRaises(model.OperationNotFoundError):

botocore/client.py
@@ -31,7 +31,6 @@ from botocore.utils import S3RegionRedirector
 from botocore.utils import fix_s3_host
 from botocore.utils import switch_to_virtual_host_style
 from botocore.utils import S3_ACCELERATE_WHITELIST
-from botocore.utils import get_service_event_name
 from botocore.args import ClientArgsCreator
 from botocore.compat import urlsplit
 from botocore import UNSIGNED
@@ -91,7 +90,7 @@ class ClientCreator(object):
         py_name_to_operation_name = self._create_name_mapping(service_model)
         class_attributes['_PY_TO_OP_NAME'] = py_name_to_operation_name
         bases = [BaseClient]
-        service_id = get_service_event_name(service_model.service_id)
+        service_id = service_model.service_id.hyphenize()
         self._event_emitter.emit(
             'creating-client-class.%s' % service_id,
             class_attributes=class_attributes,
@@ -108,6 +107,8 @@ class ClientCreator(object):
 
     def _register_retries(self, client):
         endpoint_prefix = client.meta.service_model.endpoint_prefix
+        service_id = client.meta.service_model.service_id
+        service_event_name = service_id.hyphenize()
 
         # First, we load the entire retry config for all services,
         # then pull out just the information we need.
@@ -125,9 +126,11 @@ class ClientCreator(object):
                      client.meta.service_model.service_name)
         handler = self._retry_handler_factory.create_retry_handler(
             retry_config, endpoint_prefix)
-        unique_id = 'retry-config-%s' % endpoint_prefix
-        client.meta.events.register('needs-retry.%s' % endpoint_prefix,
-                                    handler, unique_id=unique_id)
+        unique_id = 'retry-config-%s' % service_event_name
+        client.meta.events.register(
+            'needs-retry.%s' % service_event_name, handler,
+            unique_id=unique_id
+        )
 
     def _register_s3_events(self, client, endpoint_bridge, endpoint_url,
                             client_config, scoped_config):
@@ -548,7 +551,7 @@ class BaseClient(object):
     def __getattr__(self, item):
 
         event_name = 'getattr.%s.%s' % (
-            get_service_event_name(self._service_model.service_id), item
+            self._service_model.service_id.hyphenize(), item
         )
         handler, event_response = self.meta.events.emit_until_response(
             event_name, client=self)
@@ -563,9 +566,11 @@ class BaseClient(object):
 
     def _register_handlers(self):
         # Register the handler required to sign requests.
-        self.meta.events.register('request-created.%s' %
-                                  self.meta.service_model.endpoint_prefix,
-                                  self._request_signer.handler)
+        service_id = self.meta.service_model.service_id.hyphenize()
+        self.meta.events.register(
+            'request-created.%s' % service_id,
+            self._request_signer.handler
+        )
 
     @property
     def _service_model(self):
@@ -591,7 +596,7 @@ class BaseClient(object):
         request_dict = self._convert_to_request_dict(
             api_params, operation_model, context=request_context)
 
-        service_id = get_service_event_name(self._service_model.service_id)
+        service_id = self._service_model.service_id.hyphenize()
         handler, event_response = self.meta.events.emit_until_response(
             'before-call.{service_id}.{operation_name}'.format(
                 service_id=service_id,
@@ -639,7 +644,7 @@ class BaseClient(object):
         # Emit an event that allows users to modify the parameters at the
         # beginning of the method. It allows handlers to modify existing
         # parameters or return a new set of parameters to use.
-        service_id = get_service_event_name(self._service_model.service_id)
+        service_id = self._service_model.service_id.hyphenize()
         responses = self.meta.events.emit(
             'provide-client-params.{service_id}.{operation_name}'.format(
                 service_id=service_id,

botocore/endpoint.py
@@ -23,7 +23,6 @@ from botocore.awsrequest import create_request_object
 from botocore.exceptions import HTTPClientError
 from botocore.httpsession import URLLib3Session
 from botocore.utils import is_valid_endpoint_url, get_environ_proxies
-from botocore.utils import get_service_event_name
 from botocore.hooks import first_non_none_response
 from botocore.history import get_global_history_recorder
 from botocore.response import StreamingBody
@@ -109,9 +108,7 @@ class Endpoint(object):
                 operation_model.has_streaming_output,
                 operation_model.has_event_stream_output
             ])
-            service_id = get_service_event_name(
-                operation_model.service_model.service_id
-            )
+            service_id = operation_model.service_model.service_id.hyphenize()
             event_name = 'request-created.{service_id}.{op_name}'.format(
                 service_id=service_id,
                 op_name=operation_model.name)
@@ -198,9 +195,7 @@ class Endpoint(object):
 
     def _needs_retry(self, attempts, operation_model, request_dict,
                      response=None, caught_exception=None):
-        service_id = get_service_event_name(
-            operation_model.service_model.service_id
-        )
+        service_id = operation_model.service_model.service_id.hyphenize()
         event_name = 'needs-retry.%s.%s' % (
             service_id,
             operation_model.name)

botocore/signers.py
@@ -23,7 +23,6 @@ from botocore.exceptions import UnknownSignatureVersionError
 from botocore.exceptions import UnknownClientMethodError
 from botocore.exceptions import UnsupportedSignatureVersionError
 from botocore.utils import fix_s3_host, datetime2timestamp
-from botocore.utils import get_service_event_name
 
 
 class RequestSigner(object):
@@ -40,7 +39,7 @@ class RequestSigner(object):
     and disabling signing per operation.
 
 
-    :type service_id: str
+    :type service_id: botocore.model.ServiceId
     :param service_id: The service id for the service, e.g. ``S3``
 
     :type region_name: string
@@ -66,7 +65,7 @@ class RequestSigner(object):
         self._signing_name = signing_name
         self._signature_version = signature_version
         self._credentials = credentials
-        self._service_event_name = get_service_event_name(service_id)
+        self._service_id = service_id
 
         # We need weakref to prevent leaking memory in Python 2.6 on Linux 2.6
         self._event_emitter = weakref.proxy(event_emitter)
@@ -130,7 +129,7 @@ class RequestSigner(object):
         # Allow mutating request before signing
         self._event_emitter.emit(
             'before-sign.{0}.{1}'.format(
-                self._service_event_name, operation_name),
+                self._service_id.hyphenize(), operation_name),
             request=request, signing_name=signing_name,
             region_name=self._region_name,
             signature_version=signature_version, request_signer=self,
@@ -180,7 +179,7 @@ class RequestSigner(object):
 
         handler, response = self._event_emitter.emit_until_response(
             'choose-signer.{0}.{1}'.format(
-                self._service_event_name, operation_name),
+                self._service_id.hyphenize(), operation_name),
             signing_name=self._signing_name, region_name=self._region_name,
             signature_version=signature_version, context=context)
 

botocore/utils.py
@@ -973,7 +973,7 @@ def deep_merge(base, extra):
         base[key] = extra[key]
 
 
-def get_service_event_name(service_id):
+def hyphenize_service_id(service_id):
     """Translate the form used for event emitters.
 
     :param service_id: The service_id to convert.

commit 9f306d327fc8d38f138d98bd0b290d426b8c0042

Improve error message with missing service id
Originally introduced in ba514720588c520fdf5ed0c77ddc76e4455246d0,
one of the tests was modified to have different behavior than intended.

The test is suppose to verify that without any of the needed
metadata properties we don't register a retry handler.
Right now, if a serviceId is missing you'll get an error message of
"NoneType" has no attribute "lower()".
With this change, you'll get the missing metadata error message
from models.py.

To help clarify this, I've changed the name of the test.  I also
added a repr to the Service class.  You now get:

$ aws s3 ls

"serviceId" not defined in the metadata of the model: ServiceModel(s3)

botocore/handlers.py
@@ -274,13 +274,13 @@ def _needs_s3_sse_customization(params, sse_member_prefix):
 def register_retries_for_service(service_data, session,
                                  service_name, **kwargs):
     loader = session.get_component('data_loader')
-    service_id = service_data.get('metadata', {}).get('serviceId')
-    service_event_name = hyphenize_service_id(service_id)
     endpoint_prefix = service_data.get('metadata', {}).get('endpointPrefix')
     if endpoint_prefix is None:
         logger.debug("Not registering retry handlers, could not endpoint "
                      "prefix from model for service %s", service_name)
         return
+    service_id = service_data.get('metadata', {}).get('serviceId')
+    service_event_name = hyphenize_service_id(service_id)
     config = _load_retry_config(loader, endpoint_prefix)
     if not config:
         return

botocore/model.py
@@ -339,6 +339,10 @@ class ServiceModel(object):
     def signature_version(self, value):
         self._signature_version = value
 
+    def __repr__(self):
+        return '%s(%s)' % (self.__class__.__name__, self.service_name)
+
+
 
 class OperationModel(object):
     def __init__(self, operation_model, service_model, name=None):

tests/unit/test_handlers.py
@@ -549,8 +549,8 @@ class TestHandlers(BaseSessionTest):
                   'UserData': b64_user_data}
         self.assertEqual(params, result)
 
-    def test_register_retry_for_handlers_with_no_endpoint_prefix(self):
-        no_endpoint_prefix = {'metadata': {'serviceId': 'foo'}}
+    def test_register_retry_for_handlers_with_no_metadata(self):
+        no_endpoint_prefix = {'metadata': {}}
         session = mock.Mock()
         handlers.register_retries_for_service(service_data=no_endpoint_prefix,
                                               session=mock.Mock(),

tests/unit/test_model.py
@@ -89,6 +89,10 @@ class TestServiceModel(unittest.TestCase):
     def test_shape_names(self):
         self.assertEqual(self.service_model.shape_names, ['StringShape'])
 
+    def test_repr_has_service_name(self):
+        self.assertEqual(repr(self.service_model),
+                         'ServiceModel(endpoint-prefix)')
+
 
 class TestOperationModelFromService(unittest.TestCase):
     def setUp(self):