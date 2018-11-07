f5a1577a5ba7b8895f1dab8521de7644b4b34fd7
stealthycoin <stealthycoin@users.noreply.github.com>
2018-11-06 16:48:40 -0800

    Add exception for sms-pinpoint-voice in endpoint test

--- tests/functional/test_endpoints.py
+++ tests/functional/test_endpoints.py
@@ -54,6 +54,7 @@ SERVICE_RENAMES = {
     'marketplace-entitlement': 'marketplace-entitlement-service',
     'meteringmarketplace': 'marketplace-metering',
     'mgh': 'migration-hub',
+    'sms-voice': 'pinpoint-sms-voice',
     'resourcegroupstaggingapi': 'resource-groups-tagging-api',
     'route53': 'route-53',
     'route53domains': 'route-53-domains',
