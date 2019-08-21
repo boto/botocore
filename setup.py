d37363ff13e758aa1d09097111a258fda193aba3
Bradley Atkins <bradley.atkins@bjss.com>
2019-08-21 11:31:41 +0100

    Removed integration tests and move dependencies to requirements.txt

--- setup.py
+++ setup.py
@@ -49,9 +49,6 @@ elif sys.version_info[:2] == (3, 3):
 else:
     requires.append('urllib3>=1.20,<1.26')
 
-requires.append('monotonic==1.5')
-requires.append('statistics==1.0.3.5')
-
 setup(
     name='botocore',
     version=find_version("botocore", "__init__.py"),
