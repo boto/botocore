a61abe0b09e2a0c5c27e3fdc8c071b1ca3c93c78
Bradley Atkins <bradley.atkins@bjss.com>
2019-08-21 13:53:34 +0100

    Moved metrics functions out of test class

--- tests/unit/test_rate_mgr.py
+++ tests/unit/test_rate_mgr.py
@@ -52,7 +52,7 @@ class TestRateManager(unittest.TestCase):
 
         self.brm.stop(True)
 
-        # self.record_test_metrics(self)
+        record_test_metrics(self)
         self.assertTrue((now_millis() - self.start) < 10000)
 
     def test_100_waiters_with_no_contention(self):
@@ -72,7 +72,7 @@ class TestRateManager(unittest.TestCase):
             t.join()
 
         self.brm.stop(True)
-        # self.record_test_metrics(self)
+        record_test_metrics(self)
 
         self.assertTrue(self.brm.queue.empty())
 
@@ -100,72 +100,75 @@ class TestRateManager(unittest.TestCase):
 
         self.brm.stop(True)
 
-        # self.record_test_metrics(self)
+        record_test_metrics(self)
         self.assertTrue(self.brm.queue.empty())
 
-    # def test_print_metric(self):
-    #     self.print_test_metrics(self)
+    def test_print_metric(self):
+        print_test_metrics(self)
 
-#     def record_test_metrics(self):
-#         total = 0
-#         for t in self.thread_end_times:
-#             total += t
-#
-#         avg = float(get_step_average(self.thread_end_times)) / 1000
-#         avg_step_str = '{0: <25}'.format('Average thread time') + "= {0:.2f}".format(avg)
-#         act_step = '{0: <25}'.format('Set step interval') + "= {0:.2f}".format(float(self.step) / 1000)
-#
-#         dev = statistics.stdev(get_intervals(self.brm.steps, 1001))
-#         std_dev = 'Step Standard Deviation = ' + str("{0:.2f}".format(dev))
-#
-#         self.test_metrics.append(
-#             self.TestMetric(
-#                 test_name=self._testMethodName,
-#                 std_dev=std_dev,
-#                 act_step=act_step,
-#                 avg_steps=avg_step_str
-#             )
-#                                  )
-#
-#     def print_test_metrics(self):
-#         for metrics in self.test_metrics:
-#             print('\n' + metrics.test_name)
-#             print('\t' + metrics.std_dev)
-#             print('\t' + metrics.act_step)
-#             print('\t' + metrics.avg_steps)
-#
-#     class TestMetric:
-#         """
-#         Record the metrics for a run
-#         """
-#         def __init__(self, **kwargs):
-#             self.test_name = kwargs['test_name']
-#             self.std_dev = kwargs['std_dev']
-#             self.act_step = kwargs['act_step']
-#             self.avg_steps = kwargs['avg_steps']
-#
-#
-# def get_step_average(steps):
-#     total = 0
-#     intervals = get_intervals(steps, 1)
-#     for interval in intervals:
-#         total = total + interval
-#
-#     size = intervals.__len__() or 1
-#     return total / size
-#
-#
-# def get_intervals(collection, divisor):
-#     high = 0
-#     intervals = []
-#     for t in reversed(collection):
-#         if high == 0:
-#             high = t
-#             continue
-#
-#         intervals.append((high - t) / divisor)
-#         high = t
-#     return intervals
+
+def record_test_metrics(self):
+    total = 0
+    for t in self.thread_end_times:
+        total += t
+
+    avg = float(get_step_average(self.thread_end_times)) / 1000
+    avg_step_str = '{0: <25}'.format('Average thread time') + "= {0:.2f}".format(avg)
+    act_step = '{0: <25}'.format('Set step interval') + "= {0:.2f}".format(float(self.step) / 1000)
+
+    dev = statistics.stdev(get_intervals(self.brm.steps, 1001))
+    std_dev = 'Step Standard Deviation = ' + str("{0:.2f}".format(dev))
+
+    self.test_metrics.append(
+        TestMetric(
+            test_name=self._testMethodName,
+            std_dev=std_dev,
+            act_step=act_step,
+            avg_steps=avg_step_str
+        )
+                             )
+
+
+def print_test_metrics(self):
+    for metrics in self.test_metrics:
+        print('\n' + metrics.test_name)
+        print('\t' + metrics.std_dev)
+        print('\t' + metrics.act_step)
+        print('\t' + metrics.avg_steps)
+
+
+class TestMetric:
+    """
+    Record the metrics for a run
+    """
+    def __init__(self, **kwargs):
+        self.test_name = kwargs['test_name']
+        self.std_dev = kwargs['std_dev']
+        self.act_step = kwargs['act_step']
+        self.avg_steps = kwargs['avg_steps']
+
+
+def get_step_average(steps):
+    total = 0
+    intervals = get_intervals(steps, 1)
+    for interval in intervals:
+        total = total + interval
+
+    size = intervals.__len__() or 1
+    return total / size
+
+
+def get_intervals(collection, divisor):
+    high = 0
+    intervals = []
+    for t in reversed(collection):
+        if high == 0:
+            high = t
+            continue
+
+        intervals.append((high - t) / divisor)
+        high = t
+    return intervals
 
 
 def now_millis():
