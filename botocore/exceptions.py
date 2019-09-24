06d6a4464cf3950bd27f1d5077781913602fdff9
Jordan Guymon <jogu.sd@gmail.com>
2019-09-24 13:30:53 -0700

    Initial support for SSO credentials
    This implements an SSO token fetcher class that will retrieve an access
    token from SSO using the device authorization flow. This token can then
    be cached and used to fetch AWS credentials.

--- botocore/exceptions.py
+++ botocore/exceptions.py
@@ -525,3 +525,26 @@ class MissingServiceIdError(UndefinedModelAttributeError):
         msg = self.fmt.format(**kwargs)
         Exception.__init__(self, msg)
         self.kwargs = kwargs
+
+
+class SSOError(BotoCoreError):
+    fmt = "An unspecified error happened when resolving SSO credentials"
+
+
+class PendingAuthorizationExpiredError(SSOError):
+    fmt = (
+        "The pending authorization to retrieve an SSO token has expired. The "
+        "device authorization flow to retrieve an SSO token must be restarted."
+    )
+
+
+class SSOTokenLoadError(SSOError):
+    fmt = "Error loading SSO Token: {error_msg}"
+
+
+class UnauthorizedSSOTokenError(SSOError):
+    fmt = (
+        "The SSO session associated with this profile has expired or is "
+        "otherwise invalid. To refresh this SSO session run aws sso login "
+        "with the corresponding profile."
+    )
