20ba659346537934c1f47a6c8541ca0e9c5fecb2
aws-sdk-python-automation <github-aws-sdk-python-automation@amazon.com>
2020-01-09 19:07:55 +0000

    Bumping version to 1.13.50

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -2,6 +2,12 @@
 CHANGELOG
 =========
 
+1.13.50
+=======
+
+* api-change:``logs``: Update logs client to latest version
+
+
 1.13.49
 =======
 
--- botocore/__init__.py
+++ botocore/__init__.py
@@ -16,7 +16,7 @@ import os
 import re
 import logging
 
-__version__ = '1.13.49'
+__version__ = '1.13.50'
 
 
 class NullHandler(logging.Handler):
