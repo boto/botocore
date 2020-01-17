b7bd204b69ba29b85583aaf9e14cb32ef0eff39f
Sebastian P <moseb@users.noreply.github.com>
2020-01-17 14:19:19 +0100

    setup.py: Allow use with docutils >=0.16

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def find_version(*file_paths):
 
 
 requires = ['jmespath>=0.7.1,<1.0.0',
-            'docutils>=0.10,<0.16',
+            'docutils>=0.10,<1.0.0',
             'python-dateutil>=2.1,<3.0.0',
             'urllib3>=1.20,<1.26']
 
