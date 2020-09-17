947fa91ef173305f6cd4f43f87e9c23ce8765a6e
Zidaan Dutta <ziddutta@amazon.com>
2020-09-17 15:24:21 -0400

    Update response.py

--- botocore/response.py
+++ botocore/response.py
@@ -47,6 +47,9 @@ class StreamingBody(IOBase):
         self._amount_read = 0
 
     def __del__(self):
+        #Extending destructor in order to preserve the underlying raw_stream.
+        #The ability to add custom cleanup logic introduced in Python3.4+.
+        #https://www.python.org/dev/peps/pep-0442/
         pass
 
     def set_socket_timeout(self, timeout):
