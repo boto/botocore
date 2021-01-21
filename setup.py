3d35b64e3e1e1eb17cd4dd34fb86ab79d112490f
Nate Prewitt <Nate.Prewitt@gmail.com>
2021-01-21 13:38:12 -0800

    Add changes for Python 3.4/3.5 removal

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ setup(
     install_requires=requires,
     extras_require={},
     license="Apache License 2.0",
+    python_requires=">= 2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
@@ -62,8 +63,6 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
