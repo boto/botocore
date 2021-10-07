89d992eefe110c5dddb9a2b13e91596296e76f19
Nate Prewitt <nate.prewitt@gmail.com>
2021-10-06 23:06:44 -0700

    Update tests to run on Windows and parallelized

--- tests/unit/response_parsing/test_response_parsing.py
+++ tests/unit/response_parsing/test_response_parsing.py
@@ -38,7 +38,37 @@ SPECIAL_CASES = [
 ]
 
 
-def _test_parsed_response(xmlfile, response_body, operation_model, expected):
+def _get_expected_parsed_result(filename):
+    dirname, filename = os.path.split(filename)
+    basename = os.path.splitext(filename)[0]
+    jsonfile = os.path.join(dirname, basename + '.json')
+    with open(jsonfile) as f:
+        return json.load(f)
+
+
+def _get_raw_response_body(xmlfile):
+    with open(xmlfile, 'rb') as f:
+        return f.read()
+
+
+def _get_operation_model(service_model, filename):
+    dirname, filename = os.path.split(filename)
+    basename = os.path.splitext(filename)[0]
+    sn, opname = basename.split('-', 1)
+    # In order to have multiple tests for the same
+    # operation a '#' char is used to separate
+    # operation names from some other suffix so that
+    # the tests have a different filename, e.g
+    # my-operation#1.xml, my-operation#2.xml.
+    opname = opname.split('#')[0]
+    operation_names = service_model.operation_names
+    for operation_name in operation_names:
+        if xform_name(operation_name) == opname.replace('-', '_'):
+            return service_model.operation_model(operation_name)
+
+
+def _test_parsed_response(xmlfile, operation_model, expected):
+    response_body = _get_raw_response_body(xmlfile)
     response = {
         'body': response_body,
         'status_code': 200,
@@ -106,10 +136,11 @@ def _convert_bytes_to_str(parsed):
 
 
 def _xml_test_cases():
+    session = create_session()
+    test_cases = []
     for dp in ['responses', 'errors']:
         data_path = os.path.join(os.path.dirname(__file__), 'xml')
         data_path = os.path.join(data_path, dp)
-        session = create_session()
         xml_files = glob.glob('%s/*.xml' % data_path)
         service_names = set()
         for fn in xml_files:
@@ -121,47 +152,20 @@ def _xml_test_cases():
             for xmlfile in service_xml_files:
                 expected = _get_expected_parsed_result(xmlfile)
                 operation_model = _get_operation_model(service_model, xmlfile)
-                raw_response_body = _get_raw_response_body(xmlfile)
-                yield xmlfile, raw_response_body, operation_model, expected
+                test_cases.append(
+                    (xmlfile, operation_model, expected)
+                )
+    return sorted(test_cases)
 
 
 @pytest.mark.parametrize(
-    "xmlfile, raw_response_body, operation_model, expected",
+    "xmlfile, operation_model, expected",
     _xml_test_cases()
 )
-def test_xml_parsing(xmlfile, raw_response_body, operation_model, expected):
+def test_xml_parsing(xmlfile, operation_model, expected):
     _test_parsed_response(
-        xmlfile, raw_response_body, operation_model, expected
+        xmlfile, operation_model, expected
     )
-
-
-def _get_raw_response_body(xmlfile):
-    with open(xmlfile, 'rb') as f:
-        return f.read()
-
-
-def _get_operation_model(service_model, filename):
-    dirname, filename = os.path.split(filename)
-    basename = os.path.splitext(filename)[0]
-    sn, opname = basename.split('-', 1)
-    # In order to have multiple tests for the same
-    # operation a '#' char is used to separate
-    # operation names from some other suffix so that
-    # the tests have a different filename, e.g
-    # my-operation#1.xml, my-operation#2.xml.
-    opname = opname.split('#')[0]
-    operation_names = service_model.operation_names
-    for operation_name in operation_names:
-        if xform_name(operation_name) == opname.replace('-', '_'):
-            return service_model.operation_model(operation_name)
-
-
-def _get_expected_parsed_result(filename):
-    dirname, filename = os.path.split(filename)
-    basename = os.path.splitext(filename)[0]
-    jsonfile = os.path.join(dirname, basename + '.json')
-    with open(jsonfile) as f:
-        return json.load(f)
 
 
 def _json_test_cases():
@@ -172,6 +176,7 @@ def _json_test_cases():
     json_responses_dir = os.path.join(base_dir, 'errors')
     expected_parsed_dir = os.path.join(base_dir, 'expected')
     session = botocore.session.get_session()
+    json_test_cases = []
     for json_response_file in os.listdir(json_responses_dir):
         # Files look like: 'datapipeline-create-pipeline.json'
         service_name, operation_name = os.path.splitext(
@@ -188,18 +193,19 @@ def _json_test_cases():
         for op_name in operation_names:
             if xform_name(op_name) == operation_name.replace('-', '_'):
                 operation_model = service_model.operation_model(op_name)
-        with open(raw_response_file, 'rb') as f:
-            raw_response_body = f.read()
-        yield raw_response_file, raw_response_body, operation_model, expected
+        json_test_cases.append(
+            (raw_response_file, operation_model, expected)
+        )
+    return sorted(json_test_cases)
 
 
 @pytest.mark.parametrize(
-    "raw_response_file, raw_response_body, operation_model, expected",
+    "raw_response_file, operation_model, expected",
     _json_test_cases()
 )
 def test_json_errors_parsing(
-    raw_response_file, raw_response_body, operation_model, expected
+    raw_response_file, operation_model, expected
 ):
     _test_parsed_response(
-        raw_response_file, raw_response_body, operation_model, expected
+        raw_response_file, operation_model, expected
     )
