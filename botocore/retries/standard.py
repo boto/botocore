ef31254d96502db058e98fcfb325fbe406356610
Zidaan Dutta <ziddutta@amazon.com>
2021-11-11 20:23:30 -0500

    Adding retry information headers

--- botocore/retries/standard.py
+++ botocore/retries/standard.py
@@ -266,6 +266,11 @@ class MaxAttemptsChecker(BaseRetryableChecker):
 
     def is_retryable(self, context):
         under_max_attempts = context.attempt_number < self._max_attempts
+        retries_context = context.request_context['retries']
+        retries_max_attempts = retries_context.setdefault(
+            'max', self._max_attempts)
+        if self._max_attempts > retries_max_attempts:
+            retries_context['max'] = self._max_attempts
         if not under_max_attempts:
             logger.debug("Max attempts of %s reached.", self._max_attempts)
             context.add_retry_metadata(MaxAttemptsReached=True)
