43b50855b5840e52adc6588d803e890d38e63ffd
Jonathan Gaytan <jonathangee09@gmail.com>
2022-08-30 10:01:25 -0700

    Removed the creation of redundant resources to improve efficiency.

--- tests/functional/test_model_completeness.py
+++ tests/functional/test_model_completeness.py
@@ -10,18 +10,20 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-import pytest
 import json
 import os
+import pytest
 
 from botocore.exceptions import DataNotFoundError
 from botocore.loaders import Loader
-from botocore.session import Session
+
+loader = Loader()
+available_services = loader.list_available_services(type_name='service-2')
 
 
 def _paginators_and_waiters_test_cases():
-    for service_name in Session().get_available_services():
-        versions = Loader().list_api_versions(service_name, 'service-2')
+    for service_name in available_services:
+        versions = loader.list_api_versions(service_name, 'service-2')
         if len(versions) > 1:
             for type_name in ['paginators-1', 'waiters-2']:
                 yield service_name, type_name, versions[-2], versions[-1]
@@ -36,7 +38,6 @@ def test_paginators_and_waiters_are_not_lost_in_new_version(
 ):
     # Make sure if a paginator and/or waiter exists in previous version,
     # there will be a successor existing in latest version.
-    loader = Loader()
     try:
         loader.load_service_model(service_name, type_name, previous_version)
     except DataNotFoundError:
@@ -51,8 +52,8 @@ def test_paginators_and_waiters_are_not_lost_in_new_version(
 
 
 def _endpoint_rule_set_cases():
-    for service_name in Session().get_available_services():
-        versions = Loader().list_api_versions(service_name, 'service-2')
+    for service_name in available_services:
+        versions = loader.list_api_versions(service_name, 'service-2')
         for version in versions:
             yield service_name, version
 
@@ -64,32 +65,31 @@ def _endpoint_rule_set_cases():
 def test_all_endpoint_rule_sets_exist(service_name, version):
     """Tests the existence of endpoint-rule-set.json for each service
     and verifies that content is present."""
-    loader = Loader()
     type_name = 'endpoint-rule-set'
-    loader.load_service_model(service_name, type_name, version)
-    full_path = os.path.join(service_name, version, type_name)
-    data = loader.load_data(full_path)
+    data = loader.load_service_model(service_name, type_name, version)
     assert len(data['rules']) >= 1
+
+
+def _endpoint_tests_cases():
+    for service_name in available_services:
+        yield service_name
 
 
 test_data_dir = os.path.join(os.path.dirname(__file__), "endpoint-rules")
 
 
-def test_all_endpoint_tests_exist():
+@pytest.mark.parametrize("service_name", _endpoint_tests_cases())
+def test_all_endpoint_tests_exist(service_name):
     """Tests the existence of endpoint-tests.json for each service
     and verifies that content is present."""
-    for service_name in Session().get_available_services():
-        file_name = 'endpoint-tests.json'
-        endpoint_tests_file = os.path.join(
-            test_data_dir, service_name, file_name
-        )
-        with open(endpoint_tests_file) as f:
-            data = json.load(f)
-            assert len(data['testCases']) >= 1
+    file_name = 'endpoint-tests.json'
+    endpoint_tests_file = os.path.join(test_data_dir, service_name, file_name)
+    with open(endpoint_tests_file) as f:
+        data = json.load(f)
+        assert len(data['testCases']) >= 1
 
 
 def test_partitions_exists():
     """Tests the existence of partitions.json and verifies that content is present."""
-    loader = Loader()
     data = loader.load_data('partitions')
-    assert len(data['partitions']) >= 1
+    assert len(data['partitions']) >= 4
