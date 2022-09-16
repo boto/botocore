4543ceb9adb0467fd939893a74176a1a885dd2ff
Jonas Neubert <jonasneu@amazon.com>
2022-09-16 11:24:24 -0600

    aws_is_virtual_hostable_bucket endpoint provider standard library function (#2762)
    * enpoints provider stdlib: is_virtual_hostable_s3_bucket
    
    * test data files for is_virtual_hostable_s3_bucket
    
    * exclude endpoint provider unit test data from linting
    
    * Refactor conditionals in aws_is_virtual_hostable_s3_bucket
    
    Co-authored-by: Nate Prewitt <nate.prewitt@gmail.com>
    
    Co-authored-by: Nate Prewitt <nate.prewitt@gmail.com>

--- botocore/endpoint_provider.py
+++ botocore/endpoint_provider.py
@@ -29,7 +29,7 @@ from string import Formatter
 from typing import NamedTuple
 
 from botocore import xform_name
-from botocore.compat import quote, urlparse
+from botocore.compat import IPV4_RE, quote, urlparse
 from botocore.exceptions import EndpointResolutionError
 from botocore.utils import (
     ArnParser,
@@ -376,6 +376,37 @@ class RuleSetStandardLibary:
         :rtype: bool
         """
         return not value
+
+    def aws_is_virtual_hostable_s3_bucket(self, value, allow_subdomains):
+        """Evaluates whether a value is a valid bucket name for virtual host
+        style bucket URLs. To pass, the value must meet the following criteria:
+        1. is_valid_host_label(value) is True
+        2. length between 3 and 63 characters (inclusive)
+        3. does not contain uppercase characters
+        4. is not formatted as an IP address
+
+        If allow_subdomains is True, split on `.` and validate
+        each component separately.
+
+        :type value: str
+        :type allow_subdomains: bool
+        :rtype: bool
+        """
+        if (
+            value is None
+            or len(value) < 3
+            or value.lower() != value
+            or IPV4_RE.match(value) is not None
+        ):
+            return False
+
+        if allow_subdomains is True:
+            return all(
+                self.aws_is_virtual_hostable_s3_bucket(label, False)
+                for label in value.split(".")
+            )
+
+        return self.is_valid_host_label(value, allow_subdomains=False)
 
 
 class BaseRule:
