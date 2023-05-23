7e97faed3297c8bcc53d233f6bd386f84380363d
jonathan343 <jonathangee09@gmail.com>
2023-05-23 15:55:50 -0700

    PR Feedback; Peserve sidebar animations.

--- botocore/docs/translator.py
+++ botocore/docs/translator.py
@@ -36,18 +36,16 @@ class BotoHTML5Translator(SphinxHTML5Translator):
     def is_implicit_heading(self, node):
         """Determines if a node is an implicit heading.
 
-        An implicit heading is represented by a paragraph node whos only
+        An implicit heading is represented by a paragraph node whose only
         child is a strong node with text that isnt in `IGNORE_IMPLICIT_HEADINGS`.
         """
-        if (
+        return (
             len(node) == 1
             and isinstance(node[0], nodes.strong)
             and len(node[0]) == 1
             and isinstance(node[0][0], nodes.Text)
             and node[0][0].astext() not in self.IGNORE_IMPLICIT_HEADINGS
-        ):
-            return True
-        return False
+        )
 
     def visit_paragraph(self, node):
         """Visit a paragraph HTML element.
