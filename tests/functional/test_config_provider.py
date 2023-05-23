249dbcd4e4618602c60376d8c53d23e4d379c4a0
Kenneth Daily <kdaily@amazon.com>
2023-05-23 12:33:41 -0700

    Shallow copy config value store and providers
    Instead of deep copying, use a shallow copy of the config value store
    when using it for creating a new client from a session. Only use a deep
    copy for updating the section provider.
    
    Update behavior of the smart defaults update functions to use a copy
    of the existing config providers in all cases. Update the logic for
    determining which new provider to create.
    
    Move unit tests for config provider smart defaults to functional tests
    and rework them to better test behavior. A mocked defaults data file is
    used to confirm that the correct values are loaded to protect against
    changes to the live data file.

--- tests/functional/test_config_provider.py
+++ tests/functional/test_config_provider.py
@@ -10,8 +10,11 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+from pathlib import Path
+
 import pytest
 
+import botocore.exceptions
 from botocore.config import Config
 from botocore.session import get_session
 
@@ -26,6 +29,13 @@ _SDK_DEFAULT_CONFIGURATION_VALUES_ALLOWLIST = (
 session = get_session()
 loader = session.get_component('data_loader')
 sdk_default_configuration = loader.load_data('sdk-default-configuration')
+
+
+def assert_client_uses_standard_defaults(client):
+    assert client.meta.config.s3['us_east_1_regional_endpoint'] == 'regional'
+    assert client.meta.config.connect_timeout == 3.1
+    assert client.meta.endpoint_url == 'https://sts.us-west-2.amazonaws.com'
+    assert client.meta.config.retries['mode'] == 'standard'
 
 
 @pytest.mark.parametrize("mode", sdk_default_configuration['base'])
@@ -45,7 +55,69 @@ def test_default_configurations_resolve_correctly():
     client = session.create_client(
         'sts', config=config, region_name='us-west-2'
     )
+    assert_client_uses_standard_defaults(client)
+
+
+@pytest.fixture
+def loader():
+    test_models_dir = Path(__file__).parent / 'models'
+    loader = botocore.loaders.Loader()
+    loader.search_paths.insert(0, test_models_dir)
+    return loader
+
+
+@pytest.fixture
+def session(loader):
+    session = botocore.session.Session()
+    session.register_component('data_loader', loader)
+    return session
+
+
+def assert_client_uses_legacy_defaults(client):
+    assert client.meta.config.s3 is None
+    assert client.meta.config.connect_timeout == 60
+    assert client.meta.endpoint_url == 'https://sts.amazonaws.com'
+    assert client.meta.config.retries['mode'] == 'legacy'
+
+
+def assert_client_uses_testing_defaults(client):
     assert client.meta.config.s3['us_east_1_regional_endpoint'] == 'regional'
-    assert client.meta.config.connect_timeout == 3.1
-    assert client.meta.endpoint_url == 'https://sts.us-west-2.amazonaws.com'
+    assert client.meta.config.connect_timeout == 9999
+    assert client.meta.endpoint_url == 'https://sts.amazonaws.com'
     assert client.meta.config.retries['mode'] == 'standard'
+
+
+class TestConfigurationDefaults:
+    def test_defaults_mode_resolved_from_config_store(self, session):
+        config_store = session.get_component('config_store')
+        config_store.set_config_variable('defaults_mode', 'standard')
+        client = session.create_client('sts', 'us-west-2')
+        assert_client_uses_testing_defaults(client)
+
+    def test_no_mutate_session_provider(self, session):
+        # Using the standard default mode should change the connect timeout
+        # on the client, but not the session
+        standard_client = session.create_client(
+            'sts', 'us-west-2', config=Config(defaults_mode='standard')
+        )
+        assert_client_uses_testing_defaults(standard_client)
+
+        # Using the legacy default mode should not change the connect timeout
+        # on the client or the session. By default the connect timeout for a client
+        # is 60 seconds, and unset on the session.
+        legacy_client = session.create_client('sts', 'us-west-2')
+        assert_client_uses_legacy_defaults(legacy_client)
+
+    def test_defaults_mode_resolved_from_client_config(self, session):
+        config = Config(defaults_mode='standard')
+        client = session.create_client('sts', 'us-west-2', config=config)
+        assert_client_uses_testing_defaults(client)
+
+    def test_defaults_mode_resolved_invalid_mode_exception(self, session):
+        with pytest.raises(botocore.exceptions.InvalidDefaultsMode):
+            config = Config(defaults_mode='invalid_default_mode')
+            session.create_client('sts', 'us-west-2', config=config)
+
+    def test_defaults_mode_resolved_legacy(self, session):
+        client = session.create_client('sts', 'us-west-2')
+        assert_client_uses_legacy_defaults(client)
