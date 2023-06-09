a8abacaf0afa60b15109598889ede8f1bd99170b
Joshua Cannon <joshdcannon@gmail.com>
2023-06-09 09:35:59 -0500

    Update auth.py

--- botocore/auth.py
+++ botocore/auth.py
@@ -815,8 +815,8 @@ class HmacV1Auth(BaseSigner):
         self, method, split, headers, expires=None, auth_path=None
     ):
         if self.credentials.token:
-            if 'X-Amz-Security-Token' in headers:
-                del headers['X-Amz-Security-Token']
+            if 'x-amz-security-token' in headers:
+                del headers['x-amz-security-token']
             headers['x-amz-security-token'] = self.credentials.token
         string_to_sign = self.canonical_string(
             method, split, headers, auth_path=auth_path
