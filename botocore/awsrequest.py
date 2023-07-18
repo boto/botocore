bbf1a10579ec45fc7abaf55e58a934b368346dac
Theodore Ni <3806110+tjni@users.noreply.github.com>
2023-07-18 00:57:42 -0700

    Handle 100 Continue in urllib3>=2.
    This maintains compatibility from urllib3 1.25.4 to urllib3 2.0.3 at
    least by moving the code in _send_request to request. In urllib3>=2,
    the _send_request function of http.client.HTTPConnection is skipped
    in favor of a custom implementation.

--- botocore/awsrequest.py
+++ botocore/awsrequest.py
@@ -84,16 +84,14 @@ class AWSConnection:
         self._expect_header_set = False
         self.response_class = self._original_response_cls
 
-    def _send_request(self, method, url, body, headers, *args, **kwargs):
+    def request(self, method, url, body=None, headers={}, *args, **kwargs):
         self._response_received = False
         if headers.get('Expect', b'') == b'100-continue':
             self._expect_header_set = True
         else:
             self._expect_header_set = False
             self.response_class = self._original_response_cls
-        rval = super()._send_request(
-            method, url, body, headers, *args, **kwargs
-        )
+        rval = super().request(method, url, body, headers, *args, **kwargs)
         self._expect_header_set = False
         return rval
 
