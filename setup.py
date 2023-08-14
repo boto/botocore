fbbe8f869e70dfa30091bebab63f9b50e3196692
Nate Prewitt <nate.prewitt@gmail.com>
2023-08-14 13:36:55 -0600

    Update classifiers and tox

--- setup.py
+++ setup.py
@@ -52,11 +52,13 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
+        "Programming Language :: Python :: 3 :: Only",
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
 )
