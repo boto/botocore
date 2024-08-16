57086ff5017edddc46c03b752099a80cb0b0366c
Kurt McKee <contactme@kurtmckee.org>
2024-08-16 12:10:18 -0500

    Resolve Python 3.12 `.utcnow()` `DeprecationWarning`

--- tests/functional/test_sts.py
+++ tests/functional/test_sts.py
@@ -39,7 +39,7 @@ class TestSTSPresignedUrl(BaseSessionTest):
     def test_presigned_url_contains_no_content_type(self):
         timestamp = datetime(2017, 3, 22, 0, 0)
         with mock.patch('botocore.auth.datetime.datetime') as _datetime:
-            _datetime.utcnow.return_value = timestamp
+            _datetime.now.return_value = timestamp
             url = self.client.generate_presigned_url('get_caller_identity', {})
 
         # There should be no 'content-type' in x-amz-signedheaders
