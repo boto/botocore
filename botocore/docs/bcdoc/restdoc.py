b759f69967d05e7763b082e04421699c0ef8497b
Alexandre Detiste <alexandre.detiste@gmail.com>
2025-02-09 15:57:50 +0100

    remove more legacy shim usage

--- botocore/docs/bcdoc/restdoc.py
+++ botocore/docs/bcdoc/restdoc.py
@@ -13,8 +13,8 @@
 import logging
 import os
 import re
+from collections import OrderedDict
 
-from botocore.compat import OrderedDict
 from botocore.docs.bcdoc.docstringparser import DocStringParser
 from botocore.docs.bcdoc.style import ReSTStyle
 
