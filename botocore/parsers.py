433eab99490eb9bc3895173ec78be783a8fcc512
jonathan343 <jonathangee09@gmail.com>
2025-02-13 14:47:47 -0500

    revert changes and ignore the IgnoreQueryParamsInResponse case

--- botocore/parsers.py
+++ botocore/parsers.py
@@ -202,10 +202,6 @@ class ResponseParser:
 
     DEFAULT_ENCODING = 'utf-8'
     EVENT_STREAM_PARSER_CLS = None
-    # This is a list of known values for the "location" key in the
-    # serialization dict. The location key tells us where in the response
-    # to parse the value. Locations not in this list will be ignored.
-    KNOWN_LOCATIONS = ('statusCode', 'header', 'headers')
 
     def __init__(self, timestamp_parser=None, blob_parser=None):
         if timestamp_parser is None:
@@ -439,12 +435,11 @@ class BaseXMLResponseParser(ResponseParser):
             return self._handle_unknown_tagged_union_member(tag)
         for member_name in members:
             member_shape = members[member_name]
-            location = member_shape.serialization.get('location')
             if (
-                location in self.KNOWN_LOCATIONS
+                'location' in member_shape.serialization
                 or member_shape.serialization.get('eventheader')
             ):
-                # All members with known locations have already been handled,
+                # All members with locations have already been handled,
                 # so we don't need to parse these members.
                 continue
             xml_name = self._member_key_name(member_shape, member_name)
