6f27253943abbf896a7a3f440a3db0ce76426bf0
veradri <veradri@amazon.com>
2025-07-14 16:28:10 -0700

    Ensure unique conditions list in policy

--- botocore/signers.py
+++ botocore/signers.py
@@ -671,8 +671,53 @@ class S3PostPresigner:
         expires_in=3600,
         region_name=None,
     ):
-        """Generates the url and the form fields used for a presigned s3 post"""
+        """Generates the url and the form fields used for a presigned s3 post
 
+        :type request_dict: dict
+        :param request_dict: The prepared request dictionary returned by
+            ``botocore.awsrequest.prepare_request_dict()``
+
+        :type fields: dict
+        :param fields: A dictionary of prefilled form fields to build on top
+            of.
+
+        :type conditions: list
+        :param conditions: A list of conditions to include in the policy. Each
+            element can be either a list or a structure. For example:
+
+            .. code:: python
+
+                [
+                    {"acl": "public-read"},
+                    {"bucket": "amzn-s3-demo-bucket"},
+                    ["starts-with", "$key", "mykey"]
+                ]
+
+        :type expires_in: int
+        :param expires_in: The number of seconds the presigned post is valid
+            for.
+
+        :type region_name: string
+        :param region_name: The region name to sign the presigned post to.
+
+        :rtype: dict
+        :returns: A dictionary with two elements: ``url`` and ``fields``.
+            Url is the url to post to. Fields is a dictionary filled with
+            the form fields and respective values to use when submitting the
+            post. For example:
+
+            .. code:: python
+
+                {
+                    'url': 'https://amzn-s3-demo-bucket.s3.amazonaws.com',
+                    'fields': {
+                        'acl': 'public-read',
+                        'key': 'mykey',
+                        'signature': 'mysignature',
+                        'policy': 'mybase64 encoded policy'
+                    }
+                }
+        """
         if fields is None:
             fields = {}
 
