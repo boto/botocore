5747d52a5cba23022603be7916e5c9a308671054
SamRemis <sjremis94@gmail.com>
2025-07-15 11:14:06 -0400

    remove plugins setting from config

--- botocore/config.py
+++ botocore/config.py
@@ -321,7 +321,6 @@ class Config:
             ('response_checksum_validation', None),
             ('account_id_endpoint_mode', None),
             ('auth_scheme_preference', None),
-            ('botocore_client_plugins', None),
         ]
     )
 
