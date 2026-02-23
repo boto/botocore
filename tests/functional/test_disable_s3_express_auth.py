c43b930717da528cabced5d9b6c0769427b47e26
Ahmed <aemous@amazon.com>
2026-02-23 15:41:47 -0500

    Update test to remove assertion on auth_type.

--- tests/functional/test_disable_s3_express_auth.py
+++ tests/functional/test_disable_s3_express_auth.py
@@ -10,90 +10,62 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
-
 import datetime
-from unittest.mock import patch
+from unittest import mock
 
 import pytest
-from dateutil.tz import tzlocal
+from dateutil.tz import tzutc
 
+from botocore.config import Config
 from tests import ClientHTTPStubber
 
 
 class TestDisableS3ExpressAuth:
+    DATE = datetime.datetime(2024, 11, 30, 23, 59, 59, tzinfo=tzutc())
     BUCKET_NAME = 'mybucket--usw2-az1--x-s3'
-    CREATE_SESSION_BODY = b'<?xml version="1.0" encoding="UTF-8"?>\n<CreateSessionResult><Credentials><AccessKeyId>test-key</AccessKeyId><Expiration>2024-12-31T23:59:59Z</Expiration><SecretAccessKey>test-secret</SecretAccessKey><SessionToken>test-token</SessionToken></Credentials></CreateSessionResult>'
-    LIST_OBJECTS_BODY = b'<?xml version="1.0" encoding="UTF-8"?>\n<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/"><Name>mybucket--usw2-az1--x-s3</Name><Prefix/><KeyCount>0</KeyCount><MaxKeys>1000</MaxKeys><EncodingType>url</EncodingType><IsTruncated>false</IsTruncated></ListBucketResult>'
+
+    CREATE_SESSION_RESPONSE = b'<?xml version="1.0" encoding="UTF-8"?>\n<CreateSessionResult><Credentials><AccessKeyId>test-key</AccessKeyId><Expiration>2024-12-31T23:59:59Z</Expiration><SecretAccessKey>test-secret</SecretAccessKey><SessionToken>test-token</SessionToken></Credentials></CreateSessionResult>'
+    LIST_OBJECTS_RESPONSE = b'<?xml version="1.0" encoding="UTF-8"?>\n<ListBucketResult xmlns="http://s3.amazonaws.com/doc/2006-03-01/"><Name>mybucket--usw2-az1--x-s3</Name><Prefix/><KeyCount>0</KeyCount><MaxKeys>1000</MaxKeys><EncodingType>url</EncodingType><IsTruncated>false</IsTruncated></ListBucketResult>'
 
     @pytest.fixture
-    def fixed_datetime(self):
-        return datetime.datetime(2024, 11, 30, 23, 59, 59, tzinfo=tzlocal())
+    def mock_datetime(self):
+        with mock.patch('datetime.datetime', spec=True) as mock_dt:
+            mock_dt.now.return_value = self.DATE
+            yield mock_dt
 
-    def test_s3_express_auth_disabled(
-        self, patched_session, monkeypatch, fixed_datetime
-    ):
-        auth_type = None
-
-        def get_auth_type(
-            signing_name, region_name, signature_version, context, **kwargs
-        ):
-            nonlocal auth_type
-            auth_type = context.get('auth_type', None)
-
-        patched_session.register(
-            'choose-signer.s3.ListObjectsV2', get_auth_type
+    def test_disable_s3_express_auth_enabled(self, patched_session, mock_datetime):
+        config = Config(
+            s3={
+                'disable_s3_express_session_auth': True
+            }
         )
-        monkeypatch.setenv('AWS_S3_DISABLE_EXPRESS_SESSION_AUTH', 'true')
-
-        with patch('botocore.credentials.datetime') as mocked_datetime:
-            mocked_datetime.datetime.now.return_value = fixed_datetime
-            s3_client = patched_session.create_client(
-                's3', region_name='us-west-2'
-            )
-
-            with ClientHTTPStubber(s3_client, strict=True) as http_stubber:
-                http_stubber.add_response(
-                    status=200, body=self.LIST_OBJECTS_BODY
-                )
-                s3_client.list_objects_v2(Bucket=self.BUCKET_NAME)
-
-                assert len(http_stubber.requests) == 1
-
-        is_s3_express = auth_type == 'v4-s3express'
-        assert not is_s3_express
-
-    def test_s3_express_auth_enabled(
-        self, patched_session, monkeypatch, fixed_datetime
-    ):
-        auth_type = None
-
-        def get_auth_type(
-            signing_name, region_name, signature_version, context, **kwargs
-        ):
-            nonlocal auth_type
-            auth_type = context.get('auth_type', None)
-
-        patched_session.register(
-            'choose-signer.s3.ListObjectsV2', get_auth_type
+        s3_client = patched_session.create_client(
+            's3',
+            config=config,
+            region_name='us-west-2',
         )
-        monkeypatch.setenv('AWS_S3_DISABLE_EXPRESS_SESSION_AUTH', 'false')
 
-        with patch('botocore.credentials.datetime') as mocked_datetime:
-            mocked_datetime.datetime.now.return_value = fixed_datetime
-            s3_client = patched_session.create_client(
-                's3', region_name='us-west-2'
-            )
+        with ClientHTTPStubber(s3_client) as stubber:
+            stubber.add_response(body=self.LIST_OBJECTS_RESPONSE)
+            s3_client.list_objects_v2(Bucket=self.BUCKET_NAME)
 
-            with ClientHTTPStubber(s3_client, strict=True) as http_stubber:
-                http_stubber.add_response(
-                    status=200, body=self.CREATE_SESSION_BODY
-                )
-                http_stubber.add_response(
-                    status=200, body=self.LIST_OBJECTS_BODY
-                )
-                s3_client.list_objects_v2(Bucket=self.BUCKET_NAME)
+        assert len(stubber.requests) == 1
 
-                assert len(http_stubber.requests) == 2
+    def test_disable_s3_express_auth_disabled(self, patched_session, mock_datetime):
+        config = Config(
+            s3={
+                'disable_s3_express_session_auth': False
+            }
+        )
+        s3_client = patched_session.create_client(
+            's3',
+            config=config,
+            region_name='us-west-2',
+        )
 
-        is_s3_express = auth_type == 'v4-s3express'
-        assert is_s3_express
+        with ClientHTTPStubber(s3_client) as stubber:
+            stubber.add_response(body=self.CREATE_SESSION_RESPONSE)
+            stubber.add_response(body=self.LIST_OBJECTS_RESPONSE)
+            s3_client.list_objects_v2(Bucket=self.BUCKET_NAME)
+
+        assert len(stubber.requests) == 2
