4f862c964f153b6004c04b752e6010902c4b8cb2
Nate Prewitt <nate.prewitt@gmail.com>
2026-05-08 11:23:57 -0600

    Add CI for Python 3.14t

--- setup.py
+++ setup.py
@@ -63,5 +63,6 @@ setup(
         'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3.13',
         'Programming Language :: Python :: 3.14',
+        'Programming Language :: Python :: Free Threading :: 2 - Beta',
     ],
 )
